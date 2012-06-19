9182c1b1381bd43f508bfbe8242efc6d7382374c
Jason Bau <jbau@stanford.edu>
2012-06-19 11:26:52 -0700

    removed the python virtualenv directory from the repo
    everyone will have to make their own, refer to README

--- django-project/django-class2go/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/jbau/projects/class2go/django-project/django-class2go/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
