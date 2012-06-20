789e85756c6f897f270a10b81d07e11b08689fd6
unknown <Sherif@Sherif-PC.(none)>
2012-06-20 15:50:15 -0700

    Adds page shell in addition to some core CSS and JS

--- django-project/c2g/views.py
+++ django-project/c2g/views.py
@@ -1,1 +1,43 @@
-# Create your views here.
+from django.http import HttpResponse
+import c2g.lib.page_factory
+import c2g.templates.core.topbars
+
+from django.contrib.sites.models import Site
+import settings
+
+SITE_URL = Site.objects.get_current().domain
+STATIC_URL = settings.STATIC_URL
+GenPageHTML = c2g.lib.page_factory.GenPageHTML
+GenTopbar = c2g.templates.core.topbars.GenTopbar
+
+### C2G Core Views ###
+
+def home(request):
+	head = {
+		'css':[],
+		'script_srcs':[],
+	}
+	
+	body = {
+		'topbar': GenTopbar({'type': 'landing_page'}),
+		
+		'content': {
+			'l': {
+				'width': 200,
+				'content': "<span style='color: red;'>Left_column... Left_column... Left_column... Left_column... Left_column... Left_column... Left_column... Left_column... Left_column... Left_column... Left_column... Left_column... Left_column... Left_column... Left_column... Left_column... Left_column... Left_column... Left_column</span>",
+			},
+			'm': {
+				#'width': 800,
+				'min-width': 400,
+				'max-width': 800,
+				'content': "<span style='color: green;'>Middle_column... Middle_column... Middle_column... Middle_column... Middle_column... Middle_column... Middle_column... Middle_column... Middle_column... Middle_column... Middle_column... Middle_column... Middle_column... Middle_column... Middle_column... Middle_column... Middle_column... Middle_column... Middle_column</span>",
+			},
+			'r': {
+				'width': 200,
+				'content': "<span style='color: red;'>Right_column... Right_column... Right_column... Right_column... Right_column... Right_column... Right_column... Right_column... Right_column... Right_column... Right_column... Right_column... Right_column... Right_column... Right_column... Right_column... Right_column... Right_column... Right_column</span>",
+			},
+		},
+	}
+	
+	html = GenPageHTML(head, body)
+	return HttpResponse(html)
--- django-project/urls.py
+++ django-project/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'class2go.views.home', name='home'),
+    url(r'^$', 'c2g.views.home'),
     # url(r'^class2go/', include('class2go.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
