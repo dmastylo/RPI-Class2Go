4c73e1fefd673e601f512befcdad1f3585c95139
Jason Bau <jbau@stanford.edu>
2012-06-20 15:18:55 -0700

    super primitive login form

---
+++ django-project/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import *
+#import django.contrib.auth.views
+
+
+urlpatterns = patterns('',
+    url(r'^$', 'accounts.views.index'),
+    url(r'^login/', 'django.contrib.auth.views.login', {'template_name':'accounts/login.html'}, name="login-form"),
+)
--- django-project/settings.py
+++ django-project/settings.py
@@ -1,6 +1,7 @@
 # Django settings for class2go project.
 
 from database import DATABASES
+from os import path
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -94,7 +95,10 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'urls'
 
+
+thispath = path.dirname(path.realpath(__file__))
 TEMPLATE_DIRS = (
+    thispath+'/templates'
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -108,11 +112,13 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
     'south',
     'c2g',
+    #'users',
+    'accounts',
 )
 
 # A sample logging configuration. The only tangible logging
