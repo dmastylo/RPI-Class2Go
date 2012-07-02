dae40799983dadd770dbebba0943e386f993a083
dcadams <dcadams@.stanford.edu>
2012-07-02 15:41:30 -0700

    Added simple list of courses.

--- django-project/courses/views.py
+++ django-project/courses/views.py
@@ -2,9 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import Context, loader
 from django.template import RequestContext
+from c2g.models import Course
 
 def all(request):
-	return render_to_response('courses/all.html', {'request': request}, context_instance=RequestContext(request))
+	course_list = Course.objects.all()
+	return render_to_response('courses/all.html', {'request': request, 'course_list': course_list}, context_instance=RequestContext(request))
 	
 def current(request):
 	return render_to_response('courses/current.html', {'request': request}, context_instance=RequestContext(request))
