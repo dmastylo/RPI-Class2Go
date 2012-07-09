b217a7f28375a714962dda88b43480aecba32e76
kelvindo <kelvin.h.do@gmail.com>
2012-07-09 13:21:20 -0700

    Finished basic homepage for individual course instances.  Will only show announcements for a specific course instance. If instance doesn't exist, 404 returned

--- django-project/courses/branches/views.py
+++ django-project/courses/branches/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render_to_response
 from django.template import Context, loader
 from django.template import RequestContext
@@ -11,6 +11,9 @@ def members(request, course_id, branch_id):
     return render_to_response('branches/members.html', {'course_id': course_id, 'branch_id': branch_id, 'request': request}, context_instance=RequestContext(request))
 
 def view(request, course_id, branch_id):
-#    course = Course.objects.get(course_prefix=course_id, course_instance=branch_id)
-#    announcement_list = course.announcement_set.all()
-    return render_to_response('branches/view.html', {'branch_id': branch_id, 'request': request}, context_instance=RequestContext(request))
+    try:
+        course = Course.objects.get(handle=course_id+"-"+branch_id)
+    except:
+        raise Http404
+    announcement_list = course.announcement_set.all().order_by('-time_created')
+    return render_to_response('branches/view.html', {'course': course, 'announcement_list': announcement_list, 'branch_id': branch_id, 'request': request}, context_instance=RequestContext(request))
