497a76e29c15cd02f312455b776310015da0a88b
kelvindo <kelvin.h.do@gmail.com>
2012-07-09 16:07:50 -0700

    sorting out some db migration probs

--- django-project/c2g/models.py
+++ django-project/c2g/models.py
@@ -15,6 +15,7 @@
 from django.db import models
 from django.contrib.auth.models import User, Group
 from django.db.models.signals import post_save
+from django.db.models.signals import pre_save
 
 class Institution(models.Model):
 #    #id = models.BigIntegerField(primary_key=True)
@@ -64,6 +65,17 @@ class Course(models.Model):
         db_table = u'c2g_courses'
 
 
+def DefineUserGroupsForCourse(sender, **kwargs):
+        instance = kwargs.get('instance')
+        instance.student_group = Group.objects.create( name="Student Group for " + instance.handle)
+        instance.instructor_group = Group.objects.create(name="Instructor Group for " + instance.handle)
+        instance.tas_group = Group.objects.create(name="TAS Group for " + instance.handle)
+        instance.readonly_tas_group = Group.objects.create(name="Readonly TAS Group for " + instance.handle)
+            
+
+pre_save.connect(DefineUserGroupsForCourse, sender=Course)
+
+
 #does additional pages need an owner?
 #There's no social network
 #here, so not every item need an owner.
@@ -95,9 +107,6 @@ class Announcement(models.Model):
     last_updated = models.DateTimeField(auto_now=True, auto_now_add=True)
     class Meta:
         db_table = u'c2g_announcements'
-
-    def __unicode__(self):
-        return self.title
 
 
 ##ASSIGNMENTS SECTION####
@@ -288,7 +297,6 @@ class UserCourseData(models.Model):
 class UserProfile(models.Model):
     #id = models.AutoField(primary_key=True)
     user = models.OneToOneField(User, db_index=True)
-    is_instructor = models.IntegerField(default=False, blank=True)
     site_data = models.TextField(blank=True)
     class Meta:
         db_table = u'c2g_user_profiles'
@@ -384,23 +392,6 @@ class VideoAnnotation(models.Model):
     class Meta:
         db_table = u'c2g_video_annotations'
 
-
-
-
-#For roles, can we make do somehow with built-in django permissions
-#what custom features do we actually need here?
-class Role(models.Model):
-    #id = models.BigIntegerField(primary_key=True)
-    course = models.ForeignKey(Course)
-    title = models.CharField(max_length=255)
-    is_staff = models.IntegerField(null=True, blank=True)
-    privileges = models.TextField(blank=True)
-    holder_ids = models.TextField(blank=True)
-    holder_count = models.BigIntegerField(null=True, blank=True)
-    time_created = models.DateTimeField(auto_now=False, auto_now_add=True)
-    last_updated = models.DateTimeField(auto_now=True, auto_now_add=True)
-    class Meta:
-        db_table = u'c2g_roles'
 
 
 class SharingPermission(models.Model):
