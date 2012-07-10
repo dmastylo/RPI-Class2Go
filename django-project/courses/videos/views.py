91960fe34a8efcaa5b43ee82dbbb23888d4c43b6
Kevin Luo <kevkluo@gmail.com>
2012-07-10 11:15:11 -0700

    my changes to video files

--- django-project/courses/videos/views.py
+++ django-project/courses/videos/views.py
@@ -3,14 +3,19 @@ from django.shortcuts import render_to_response
 from django.template import Context, loader
 from django.template import RequestContext
 
+from c2g.models import Video, VideoTopic
+
 def list(request, course_id, branch_id):
-	return render_to_response('videos/list.html', {'course_id': course_id, 'branch_id': branch_id, 'request': request}, context_instance=RequestContext(request))
+	topics = VideoTopic.objects.all()
+	return render_to_response('videos/list.html', {'course_id': course_id, 'branch_id': branch_id, 'topics': topics, 'request': request}, context_instance=RequestContext(request))
+	#return render_to_response('videos/list.html', {'course_id': course_id, 'branch_id': branch_id, 'request': request}, context_instance=RequestContext(request))
 	
 def admin(request, course_id, branch_id):
 	return render_to_response('videos/admin.html', {'course_id': course_id, 'branch_id': branch_id, 'request': request}, context_instance=RequestContext(request))
 	
 def view(request, course_id, branch_id, video_id):
-	return render_to_response('videos/view.html', {'course_id': course_id, 'branch_id': branch_id, 'video_id': video_id, 'request': request}, context_instance=RequestContext(request))
+	video = Video.objects.get(id=video_id)
+	return render_to_response('videos/view.html', {'course_id': course_id, 'branch_id': branch_id, 'video': video, 'request': request}, context_instance=RequestContext(request))
 	
 def edit(request, course_id, branch_id, video_id):
 	return render_to_response('videos/edit.html', {'course_id': course_id, 'branch_id': branch_id, 'video_id': video_id, 'request': request}, context_instance=RequestContext(request))
