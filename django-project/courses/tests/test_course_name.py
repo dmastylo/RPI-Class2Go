d2ba97e5de68f8e33fbd708df940cb786a66983d
Sef Kloninger <sef@kloninger.com>
2012-07-19 12:26:25 -0700

    Test at the request level instead of the template.
    Treats page as a black box at request/response level, instead of
    rendering the template ourselves.  This way we're testing how the
    templates are being used, not the templating system.

--- django-project/courses/tests/test_course_name.py
+++ django-project/courses/tests/test_course_name.py
@@ -1,13 +1,15 @@
 from django.test import TestCase
 from c2g.models import Institution, Course
 from django.contrib.auth.models import Group 
-from django.template.loader import render_to_string
 from db_test_data.management.commands.db_populate import Command 
+from django.test.client import RequestFactory  
+from courses.views import *
 
 class SimpleTest(TestCase):
 
     course_prefix="nlp"
     course_suffix="fall2012"
+    course_url="/"+course_prefix+"/"+course_suffix
     course_name="Natural Language Processing"
     course_title_search_string="<h2>"+course_name+"</h2>"
 
@@ -15,26 +17,15 @@ class SimpleTest(TestCase):
     pop_command = Command()
     def setUp(self):
         self.pop_command.handle()
+        self.factory = RequestFactory()
+
     def tearDown(self):
         pass
 
-    def render_and_check_template(self, templ):
-        "Render a template and see if the course title is in a header element."
-        try:
-            course = Course.objects.get(handle=self.course_prefix+"-"+self.course_suffix)
-        except:
-            self.fail("Couldn't lookup test course")
-        template_vars= {'course_prefix': self.course_prefix, 
-                'course_suffix': self.course_suffix, 
-                'course': course}
-        page = render_to_string(templ, template_vars)
-        self.assertRegexpMatches(page, self.course_title_search_string, msg="Couldn't find course header (%s) in %s" % (self.course_title_search_string, templ))
+    def test_homepage_course_title(self):
+        request = self.factory.get(self.course_url)
+        response = view(request, self.course_prefix, self.course_suffix)
+        self.assertRegexpMatches(response.content, 
+                self.course_title_search_string,
+                msg="Couldn't find name of course in the page")
 
-    # Test Cases
-
-    def test_course_title_homepage(self):
-        "Iterate through all templates and test each one"
-        targets = ['courses/view.html'] 
-        for templ in targets:
-            self.render_and_check_template(templ)
-        
