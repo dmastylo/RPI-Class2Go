47dec7433888fcd2fa9eaa4e71ebd3408400bec3
Sef Kloninger <sef@kloninger.com>
2012-07-19 15:30:31 -0700

    Pass course object along so the course title renders correctly.
    Fixes #137.  Tested in courses/tests/test_course_name.py

--- django-project/courses/videos/views.py
+++ django-project/courses/videos/views.py
@@ -2,19 +2,52 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import Context, loader
 from django.template import RequestContext
-
-from c2g.models import Video, VideoTopic
+from c2g.models import Course, Video, VideoTopic
 
 def list(request, course_prefix, course_suffix):
-	topics = VideoTopic.objects.all()
-	return render_to_response('videos/list.html', {'course_prefix': course_prefix, 'course_suffix': course_suffix, 'topics': topics, 'request': request}, context_instance=RequestContext(request))
-	
+    course = Course.objects.get(handle=course_prefix+"-"+course_suffix)
+    topics = VideoTopic.objects.all()
+    return render_to_response('videos/list.html', 
+            {'request': request,
+             'course_prefix': course_prefix, 
+             'course_suffix': course_suffix, 
+             'course': course, 
+             'topics': topics, 
+             }, 
+            context_instance=RequestContext(request))
+    
 def admin(request, course_prefix, course_suffix):
-	return render_to_response('videos/admin.html', {'course_prefix': course_prefix, 'course_suffix': course_suffix, 'request': request}, context_instance=RequestContext(request))
-	
+    course = Course.objects.get(handle=course_prefix+"-"+course_suffix)
+    return render_to_response('videos/admin.html', 
+            {'request': request,
+             'course_prefix': course_prefix, 
+             'course_suffix': course_suffix, 
+             'course': course, 
+             }, 
+            context_instance=RequestContext(request))
+    
 def view(request, course_prefix, course_suffix, video_id):
-	video = Video.objects.get(id=video_id)
-	return render_to_response('videos/view.html', {'course_prefix': course_prefix, 'course_suffix': course_suffix, 'video': video, 'request': request}, context_instance=RequestContext(request))
-	
+    course = Course.objects.get(handle=course_prefix+"-"+course_suffix)
+    video = Video.objects.get(id=video_id)
+    return render_to_response('videos/view.html', {'course_prefix': course_prefix, 'course_suffix': course_suffix, 'video': video, 'request': request}, context_instance=RequestContext(request))
+    return render_to_response('videos/view.html', 
+            {'request': request,
+             'course_prefix': course_prefix, 
+             'course_suffix': course_suffix, 
+             'course': course, 
+             'video': video, 
+             }, 
+            context_instance=RequestContext(request))
+    
 def edit(request, course_prefix, course_suffix, video_id):
-	return render_to_response('videos/edit.html', {'course_prefix': course_prefix, 'course_suffix': course_suffix, 'video_id': video_id, 'request': request}, context_instance=RequestContext(request))
+    course = Course.objects.get(handle=course_prefix+"-"+course_suffix)
+    return render_to_response('videos/edit.html', 
+            {'request': request,
+             'course_prefix': course_prefix, 
+             'course_suffix': course_suffix, 
+             'course': course, 
+             'video_id': video_id, 
+             }, 
+            context_instance=RequestContext(request))
+
+
