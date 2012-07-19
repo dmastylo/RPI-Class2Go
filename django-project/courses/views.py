5929bcee92a75513c1bd7ccc20a24c1685b684da
Sef Kloninger <sef@kloninger.com>
2012-07-19 15:35:31 -0700

    Formatting to make template parameters more standard.
    Easier to see what's getting passed where when on their own lines.  Also
    standardize ordering.

--- django-project/courses/views.py
+++ django-project/courses/views.py
@@ -11,18 +11,38 @@ def main(request, course_prefix, course_suffix):
         raise Http404
     announcement_list = course.announcement_set.all().order_by('-time_created')
     news_list = course.newsevent_set.all().order_by('-time_created')[0:5]
-    return render_to_response('courses/view.html', {'course_prefix': course_prefix, 'course_suffix': course_suffix, 'course': course, 'announcement_list': announcement_list, 'news_list': news_list, 'request': request}, context_instance=RequestContext(request))
+    return render_to_response('courses/view.html', 
+            {'request': request,
+             'course_prefix': course_prefix, 
+             'course_suffix': course_suffix, 
+             'course': course, 
+             'announcement_list': announcement_list, 
+             'news_list': news_list, 
+             }, 
+            context_instance=RequestContext(request))
 
 def overview(request, course_prefix, course_suffix):
     try:
         course = Course.objects.get(handle=course_prefix+"-"+course_suffix)
     except:
         raise Http404
-    return render_to_response('courses/overview.html', {'course_prefix': course_prefix, 'course_suffix': course_suffix, 'course': course, 'request': request}, context_instance=RequestContext(request))
+    return render_to_response('courses/overview.html', 
+            {'request': request,
+             'course_prefix': course_prefix, 
+             'course_suffix': course_suffix, 
+             'course': course, 
+             }, 
+            context_instance=RequestContext(request))
 
 def syllabus(request, course_prefix, course_suffix):
     try:
         course = Course.objects.get(handle=course_prefix+"-"+course_suffix)
     except:
         raise Http404
-    return render_to_response('courses/syllabus.html', {'course_prefix': course_prefix, 'course_suffix': course_suffix, 'course': course, 'request': request}, context_instance=RequestContext(request))
+    return render_to_response('courses/syllabus.html', 
+            {'request': request,
+             'course_prefix': course_prefix, 
+             'course_suffix': course_suffix, 
+             'course': course,
+             }, 
+            context_instance=RequestContext(request))
