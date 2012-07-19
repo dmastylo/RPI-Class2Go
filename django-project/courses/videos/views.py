76a49671c804182a4f08b1c02ab7f41e523a818d
Kevin Luo <kevkluo@gmail.com>
2012-07-19 16:10:59 -0700

    about to merge with sef's changes

--- django-project/courses/videos/views.py
+++ django-project/courses/videos/views.py
@@ -3,13 +3,17 @@ from django.shortcuts import render_to_response
 from django.template import Context, loader
 from django.template import RequestContext
 
-from c2g.models import Course, Video, VideoTopic
+from c2g.models import Course, Video, VideoTopic, VideoActivity
 
 def list(request, course_prefix, course_suffix):
-        course = Course.objects.get(handle=course_prefix+"-"+course_suffix)
-        topics = course.videotopic_set.all()
-        videos = course.video_set.all()
-        return render_to_response('videos/list.html', {'course_prefix': course_prefix, 'course_suffix': course_suffix, 'topics': topics, 'videos': videos, 'request': request}, context_instance=RequestContext(request))
+	if not request.user.is_authenticated() :
+          return render_to_response('videos/list.html', {'course_prefix': course_prefix, 'course_suffix': course_suffix, 'request': request}, context_instance=RequestContext(request))
+        else:
+	  course = Course.objects.get(handle=course_prefix+"-"+course_suffix)
+          topics = course.videotopic_set.all()
+          #videos = course.video_set.all()
+          videoRecs = request.user.videoactivity_set.filter(course=course)
+	  return render_to_response('videos/list.html', {'course_prefix': course_prefix, 'course_suffix': course_suffix, 'topics': topics, 'videoRecs': videoRecs, 'request': request}, context_instance=RequestContext(request))
 
 def admin(request, course_prefix, course_suffix):
 	return render_to_response('videos/admin.html', {'course_prefix': course_prefix, 'course_suffix': course_suffix, 'request': request}, context_instance=RequestContext(request))
@@ -24,7 +28,7 @@ def edit(request, course_prefix, course_suffix, video_id):
 def save(request):
 	video_id = request.POST['video_id']
 	playTime = request.POST['playTime']
-	video = Video.objects.get(id=video_id)
+	video = VideoActivity.objects.get(student=request.user, video=video_id)
 	video.start_seconds = playTime
 	video.save()
 	return HttpResponse("saved")
