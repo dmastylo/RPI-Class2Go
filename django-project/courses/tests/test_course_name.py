70a75fd250c068e9456dd713f9b647757b81509a
Sef Kloninger <sef@kloninger.com>
2012-07-19 14:34:16 -0700

    Add test cases for videos, problemsets views

--- django-project/courses/tests/test_course_name.py
+++ django-project/courses/tests/test_course_name.py
@@ -1,9 +1,11 @@
 from django.test import TestCase
 from c2g.models import Institution, Course
-from django.contrib.auth.models import Group 
+from django.contrib.auth.models import Group, AnonymousUser
 from db_test_data.management.commands.db_populate import Command 
 from django.test.client import RequestFactory  
-from courses.views import *
+import courses.views
+import courses.videos.views
+import problemsets.views
 
 class SimpleTest(TestCase):
 
@@ -22,10 +24,22 @@ class SimpleTest(TestCase):
     def tearDown(self):
         pass
 
-    def test_homepage_course_title(self):
+    def request_and_search(self, viewname):
         request = self.factory.get(self.course_url)
-        response = view(request, self.course_prefix, self.course_suffix)
+        request.user = AnonymousUser()
+        request.session = {}
+        response = viewname(request, self.course_prefix, self.course_suffix)
         self.assertRegexpMatches(response.content, 
                 self.course_title_search_string,
-                msg="Couldn't course name in page")
+                msg="Couldn't find course name in from view '%s'" % viewname.__name__)
 
+    def test_coursename_header_for_course_views(self):
+        self.request_and_search(courses.views.main)
+        self.request_and_search(courses.views.overview)
+        self.request_and_search(courses.views.syllabus)
+
+    def test_coursename_header_for_video_views(self):
+        self.request_and_search(courses.videos.views.list)
+
+    def test_coursename_header_for_problemset_views(self):
+        self.request_and_search(problemsets.views.list)
