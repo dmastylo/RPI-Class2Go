a05b6b3efe7b26e836ba8b389dc59aa49b16333e
Sef Kloninger <sef@kloninger.com>
2012-07-25 01:00:50 -0700

    Remove hard-coded db location
    Vestige of prior runs, wasn't being used anyway.

--- chef/cookbooks/class2go-database.py/recipes/default.rb
+++ chef/cookbooks/class2go-database.py/recipes/default.rb
@@ -10,10 +10,6 @@
 template "database.py" do
     path "/home/bitnami/class2go/django-project/database.py"
     source "database.py.erb"
-    variables(
-        # TODO - should come from environment
-        :database_host => "c2g-stage-appdb1.czjqjb57rejd.us-west-2.rds.amazonaws.com"
-    )
     owner "bitnami"
     group "bitnami"
     mode "0644"
