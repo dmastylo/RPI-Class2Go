fc799603d15e92a8c10787dab5f241d5bdb0cf93
Sef Kloninger <sef@kloninger.com>
2012-07-25 16:02:06 -0700

    Apache restart on its own; use bitnami idiom
    Use the bitnami idiom for controlling apache -- avoids permission
    problems we were seeing earlier.  Also factor into its own cookbook to
    be more explicit about ordering.

---
+++ chef/cookbooks/class2go-bitnami-apache-restart/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: class2go-bitnami-apache-restart
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+execute "restart-apache" do
+    command "/opt/bitnami/ctlscript.sh restart apache"
+    user "root"
+end
