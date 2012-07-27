718741d3c4d9a71e19c4579a2cc1f30c927fea0f
Sef Kloninger <sef@kloninger.com>
2012-07-27 00:45:06 -0700

    Remove chef-client role (fix #176)
    Chef-client automatically runs all the recipes that need running.  Since
    we currently have a dumb c2g deploy recipe that just syncs whatever we
    have from head, this means that both staging and prod would get
    automatically updated with HEAD -- not good.
    
    Until we have a better way to control releases, don't run chef-client
    automatically.  You can always ssh in and do a "sudo chef-client"
    yourself, don't fret.

--- chef/roles/appserver.rb
+++ chef/roles/appserver.rb
@@ -1,12 +1,18 @@
 name "appserver"
 description "Class2Go appserver node"
 
-override_attributes({ 
-    "chef-client" => {"interval" => "300"}
-})
+# on't auto-run chef-client recipe until I code up proper software
+# deployment tooling (see issue #196)
+
+#override_attributes({ 
+#    "chef-client" => {
+#                "interval" => "300",
+#                "init_style" => "none"
+#        }
+#})
 
 run_list(
-    "recipe[chef-client]",
+#   "recipe[chef-client]",
     "recipe[gdata]",
     "recipe[class2go-base]",
     "recipe[class2go-bitnami-django]",
