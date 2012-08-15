8ff3ce5811c3ee581d21e3235cdf88c7927b16f8
Sef Kloninger <sef@kloninger.com>
2012-08-15 17:54:17 -0500

    Guidelines for setting up your environments
    Include new settings for debug, admin.

--- chef/environments/example-prod.rb
+++ chef/environments/example-prod.rb
@@ -1,10 +1,16 @@
 name "prod"
 description "Class2Go Production Environment"
 
-default_attributes "aws" => {  
-    "database_host" => "prod.AAAAAAAAAAAA.us-west-2.rds.amazonaws.com",
-    "storage_bucket" => "prod-c2g",
-    "access_key" => "BBBBBBBBBBBBBBBBBBBB",
-    "access_secret" => "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC"
-}
+default_attributes \
+    "aws" => {
+        "database_host" => "prod.AAAAAAAAAAAA.us-west-2.rds.amazonaws.com",
+        "storage_bucket" => "prod-c2g",
+        "access_key" => "BBBBBBBBBBBBBBBBBBBB",
+        "access_secret" => "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC"
+    },
+    "class2go" => {
+        "production" => "True",
+        "admin_name" => "Class2Go",
+        "admin_email" => "c2gops@gmail.com"
+    }
 
