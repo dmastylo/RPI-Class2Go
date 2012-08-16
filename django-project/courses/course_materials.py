f584d8222031bcffd418bec7d2321af8fd34785f
kelvindo <kelvin.h.do@gmail.com>
2012-08-16 11:23:41 -0700

    Fixed problemset progress bar
    Problemset progress bar will now display progress and score upon
    completion

--- django-project/courses/course_materials.py
+++ django-project/courses/course_materials.py
@@ -12,12 +12,12 @@ def get_course_materials(common_page_data, get_video_content=False, get_pset_con
         index = 0
         for section in sections:
             section_dict = {'section':section, 'items':[]}
-            
+
             if get_additional_page_content:
                 for page in pages:
                     if page.section_id == section.id:
                         item = {'type':'additional_page', 'additional_page':page, 'index':page.index}
-                        
+
                         if common_page_data['course_mode'] == 'staging':
                             prod_page = page.image
                             if not prod_page.live_datetime:
@@ -34,9 +34,9 @@ def get_course_materials(common_page_data, get_video_content=False, get_pset_con
                                     visible_status = "<span style='color:green;'>Open</span>"
 
                             item['visible_status'] = visible_status
-                            
+
                         section_dict['items'].append(item)
-            
+
             if get_video_content:
                 for video in videos:
                     #if video.section_id == section.id and (common_page_data['course_mode'] == 'staging' or (video.live_datetime and video.live_datetime < common_page_data['effective_current_datetime'])):
@@ -90,36 +90,36 @@ def get_course_materials(common_page_data, get_video_content=False, get_pset_con
                                     visible_status = "<span style='color:green;'>Open</span>"
 
                             item['visible_status'] = visible_status
-#                        else:
-#                            exercises = problem_set.exercise_set.all()
-#                            numQuestions = len(exercises)
-#                             #Starting values are total questions and will be subtracted from
-#                            numCompleted = numQuestions
-#                            numCorrect = numQuestions
-#                            for exercise in exercises:
-#                                attempts = exercise.problemactivity_set.filter(student = common_page_data['request'].user).exclude(attempt_content="hint")
+                        else:
+                            psetToExs = ProblemSetToExercise.objects.getByProblemset(problem_set)
+                            numQuestions = len(psetToExs)
+                             #Starting values are total questions and will be subtracted from
+                            numCompleted = numQuestions
+                            numCorrect = numQuestions
+                            for psetToEx in psetToExs:
+                                attempts = psetToEx.problemactivity_set.filter(student = common_page_data['request'].user).exclude(attempt_content="hint")
 
-#                               #Counts the completed problems by subtracting all without a student activity recorded for it
-#                                if len(attempts) == 0:
-#                                    numCompleted -= 1
+                               #Counts the completed problems by subtracting all without a student activity recorded for it
+                                if len(attempts) == 0:
+                                    numCompleted -= 1
 
-#                                #Add one to the number of correctly answered questions if the first attempt is correct
-#                                attempts.filter(attempt_number = 1)
-#                                for attempt in attempts:
-#                                    if attempt.complete == 0:
-#                                        numCorrect -= 1
-#                                        break
+                                #Add one to the number of correctly answered questions if the first attempt is correct
+                                attempts.filter(attempt_number = 1)
+                                for attempt in attempts:
+                                    if attempt.complete == 0:
+                                        numCorrect -= 1
+                                        break
 
-#                            #Divide by zero safety check
-#                            if numQuestions == 0:
-#                                progress = 0
-#                            else:
-#                                progress = 100.0*numCompleted/numQuestions
+                            #Divide by zero safety check
+                            if numQuestions == 0:
+                                progress = 0
+                            else:
+                                progress = 100.0*numCompleted/numQuestions
 
-#                            item['numQuestions'] = numQuestions
-#                            item['numCompleted'] = numCompleted
-#                            item['numCorrect'] = numCorrect
-#                            item['progress'] = progress
+                            item['numQuestions'] = numQuestions
+                            item['numCompleted'] = numCompleted
+                            item['numCorrect'] = numCorrect
+                            item['progress'] = progress
 
                         section_dict['items'].append(item)
 
