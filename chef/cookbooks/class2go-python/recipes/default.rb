7e17ce00889f0fa02a7f63e5054caf76dedbcd74
Sef Kloninger <sef@kloninger.com>
2012-08-18 14:56:00 -0500

    apply storages backend patch (#384)
    Patch storages/backend/s3boto.py.  See issue #70 as to why this is
    needed. This is done pretty fragile as of now, w/ hardcoded package
    location and version. Should be made more robust later.

--- chef/cookbooks/class2go-python/recipes/default.rb
+++ chef/cookbooks/class2go-python/recipes/default.rb
@@ -22,3 +22,26 @@ execute "install_boto" do
     user "root"
 end
 
+
+# We need to patch the storage boto backend (see issue #70)
+# I think it's OK to leave the patch file around 
+
+cookbook_file "/tmp/s3boto_response_header_issue70.patch" do
+    source "s3boto_response_header_issue70.patch"
+    owner "root"
+    group "root"
+    mode 0644
+    action :create
+end
+
+execute "patch_storages_boto_backend" do
+    cwd "/opt/bitnami/python/lib/python2.7/site-packages/django_storages-1.1.5-py2.7.egg/storages/backends"
+    user "root"
+    # -N to patch makes it ignore patches already applied.  Even then if one
+    # patch returns 1 if a block is skipped -- returns 2 if somehing really
+    # bad happens.  So allow 1 as an OK return code
+    command "patch -N < /tmp/s3boto_response_header_issue70.patch"
+    returns [0, 1]
+    action :run
+end
+
