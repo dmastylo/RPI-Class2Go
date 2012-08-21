0d38a3751cbb5d90a0b46edc7ab6497923328e18
Jason Bau <jbau@stanford.edu>
2012-08-21 15:17:39 -0700

    Starting a branch here.  Created Middleware wrapper around get_common_page_data that adds the common page data to the request object
    Also changed courses/views.py/main to test this code

---
+++ django-project/courses/common_page_data_middleware.py
@@ -0,0 +1,23 @@
+from courses.common_page_data import get_common_page_data
+from django.http import Http404
+
+import logging
+logger = logging.getLogger(__name__)
+
+class common_data(object):
+    """
+    This is a middleware class that eagerly retrieves page data that might be common to most requests.  It is 
+    basically a wrapper around Sherif's implementation of common_page_data.  The data is stored as a field of 
+    request.
+    """
+    def process_view (self, request, view_func, view_args, view_kwargs):
+        if (('course_prefix' not in view_kwargs) or 
+            ('course_suffix' not in view_kwargs)):
+            return None # Bail out in this case since we can't find the course
+        try:
+            request.common_page_data=get_common_page_data(request, view_kwargs['course_prefix'], 
+                                                      view_kwargs['course_suffix'])
+            #logger.info('Ran get_common_page_data course: ' + request.common_page_data['course_mode'])
+            return None
+        except:
+            raise Http404
