7a438d91594cd8624fb1284758d5cf09ab5973ec
Sef Kloninger <sef@kloninger.com>
2012-08-24 14:12:37 -0700

    More lining up our settings with Piazzas

--- django-project/courses/forums/views.py
+++ django-project/courses/forums/views.py
@@ -29,33 +29,37 @@ def view(request, course_prefix, course_suffix):
     except:
         raise Http404
 
+    # import pdb; pdb.set_trace()
     lti_params = {
         "lti_message_type": "basic-lti-launch-request",
         "lti_version": "LTI-1p0",
-
         "resource_link_id": "120988f929-274612",
         "resource_link_title": "Weekly Blog",
         "resource_link_description": "A weekly blog.",
         "user_id": "8321264",
         "lis_person_contact_email_primary": request.user.email,
-        "lis_person_sourcedid": "school.edu:user",
-        "tool_consumer_instance_description": "University of School (LMSng)",
+        "lis_person_sourcedid": "stanford.edu:user",
+        "tool_consumer_instance_description": "Stanford University",
         "launch_presentation_return_url": "http://example.com/lti_return",
         "ext_lti_message_type": "extension-lti-launch",
         "ext_submit": "Press to Launch",
-        "roles": 'Instructor',
-        "context_label": "LTI 101",
+        "context_label": common_page_data['course_prefix'] + "-" + common_page_data['course_suffix'],
         "context_id": "47264",
         "context_title": "foo",
         "context_type": "bar",
     }
+    # TODO: better test? Different roles for TA's?
+    if request.user.is_staff:
+        lti_params['roles'] = "Instructor"
+    else:
+        lti_params['roles'] = "Student"
 
-    # settings for OAuth
+    # Use OAuthSimple to sign the request. Signature just ends up as a few extra parameters 
+    # passed along in the form.
     signatures = {
-        "consumer_key": database_dot_py_config.PIAZZA_KEY, 
-        "shared_secret": database_dot_py_config.PIAZZA_SECRET
+        'consumer_key': database_dot_py_config.PIAZZA_KEY, 
+        'shared_secret': database_dot_py_config.PIAZZA_SECRET,
     }
-    
     oauthsimple = OAuthSimple()
     signed_request = oauthsimple.sign({
         'path': database_dot_py_config.PIAZZA_ENDPOINT,
@@ -68,9 +72,9 @@ def view(request, course_prefix, course_suffix):
 
     form = PiazzaAuthForm(initial=lti_params)
 
-    return render_to_response('forums/piazza.html', 
-                {'common_page_data': common_page_data, 
-                'form': form, 
-                'piazza_target_url': database_dot_py_config.PIAZZA_ENDPOINT},
-            context_instance=RequestContext(request))
+    return render_to_response('forums/piazza.html', {
+            'common_page_data': common_page_data, 
+            'form': form, 
+            'piazza_target_url': database_dot_py_config.PIAZZA_ENDPOINT
+        }, context_instance=RequestContext(request))
 
