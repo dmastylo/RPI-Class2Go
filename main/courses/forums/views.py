dmastylo/RPI-Class2Go

commit e07f4959536407a919ad2258cd715009eed9ae6b

django-project --> main for the forum view (#125)

main/courses/forums/views.py
@@ -13,10 +13,7 @@ import urllib
 from courses.forums.forms import PiazzaAuthForm
 from django.contrib.auth.models import User
 
-# won't be needed once we remove the dash from the directory name
-# see https://github.com/Stanford-Online/class2go/issues/125
-database_dot_py_config = __import__('django-project.database', globals(), locals(), 
-        ['PIAZZA_ENDPOINT', 'PIAZZA_KEY', 'PIAZZA_SECRET'], -1)
+from main.database import PIAZZA_ENDPOINT, PIAZZA_KEY, PIAZZA_SECRET
 
 @auth_view_wrapper	
 def admin(request, course_prefix, course_suffix):

commit 9a823bc3b54da9004ef7b7e5863a2acc5d9109a5

Fix imported variable naming; better test for Instructor vs. Student
More fallout from #125, django-project --> main.  Instructor vs. student
should be set by the is_course_admin setting done within
common_course_data instead of explicit model testing.  More consistent.

main/courses/forums/views.py
@@ -45,8 +45,7 @@ def view(request, course_prefix, course_suffix):
         "context_title": "foo",
         "context_type": "bar",
     }
-    # TODO: better test? Different roles for TA's?
-    if request.user.is_staff:
+    if common_page_data['is_course_admin']:
         lti_params['roles'] = "Instructor"
     else:
         lti_params['roles'] = "Student"
@@ -54,12 +53,12 @@ def view(request, course_prefix, course_suffix):
     # Use OAuthSimple to sign the request. Signature just ends up as a few extra parameters 
     # passed along in the form.
     signatures = {
-        'consumer_key': database_dot_py_config.PIAZZA_KEY, 
-        'shared_secret': database_dot_py_config.PIAZZA_SECRET,
+        'consumer_key': PIAZZA_KEY, 
+        'shared_secret': PIAZZA_SECRET,
     }
     oauthsimple = OAuthSimple()
     signed_request = oauthsimple.sign({
-        'path': database_dot_py_config.PIAZZA_ENDPOINT,
+        'path': PIAZZA_ENDPOINT,
         'action': "POST",
         'parameters': lti_params, 
         'signatures': signatures,
@@ -72,6 +71,6 @@ def view(request, course_prefix, course_suffix):
     return render_to_response('forums/piazza.html', {
             'common_page_data': common_page_data, 
             'form': form, 
-            'piazza_target_url': database_dot_py_config.PIAZZA_ENDPOINT
+            'piazza_target_url': PIAZZA_ENDPOINT,
         }, context_instance=RequestContext(request))
 