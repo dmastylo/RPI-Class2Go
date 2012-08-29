19fb27b1ce0965a3306e5d6525b0cb8a927da5b0
Sef Kloninger <sef@kloninger.com>
2012-08-28 17:58:21 -0700

    New cookbook for packages kelvinator relies on (#468)

---
+++ chef/cookbooks/class2go-kelvinator/recipes/default.rb
@@ -0,0 +1,27 @@
+package "python-setuptools" do
+    action :install
+end
+
+package "python-pip" do
+    action :install
+end
+
+package "python-dev" do
+    action :install
+end
+
+package "python-numpy" do
+    action :install
+end
+
+package "python-scipy" do
+    action :install
+end
+
+package "ffmpeg" do
+    action :install
+end
+
+package "s3cmd" do
+    action :install
+end
