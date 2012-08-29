8ab7cfc358efe87260312bbd856620bf022872bf
Sef Kloninger <sef@kloninger.com>
2012-08-29 14:15:45 -0700

    Util app: add "nobody home" page at root; template subdir

--- util/urls.py
+++ util/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
+    url('^$',       'kelvinator.views.nobodyhome'),
     url('^_health', 'kelvinator.views.healthcheck'),
     url('^extract', 'kelvinator.views.extract'),
 )
