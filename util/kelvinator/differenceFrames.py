604e8c011853fcd933e90ea644464017191db663
Sef Kloninger <sef@kloninger.com>
2012-08-29 11:47:05 -0700

    Simpler image differencing to remove scipy/numpy dependency (#470)
    The old way used some fancy math to compare images -- convert to
    grayscale, normalize, etc.  To do that required the numpy/scipy
    libraries that are a pain to get working on different machines (macs,
    non-ubuntu AMI's).
    
    This just does a simpler RMS calculation over the image.  Empirically
    this seems to notice about the same number of differences, ie. cause the
    same granularity of key frames to show up.
    
    The thresholds will be different now.  15 was a good one before,
    now 1000.  Again, determined empirically, and on just a couple of
    videos (should look at more).  Force frame extraction to always
    output 640x480 sized frames to make this as consistent as possible.
    
    Also the logic for running locally was broken.  Fixed that too.

--- util/kelvinator/differenceFrames.py
+++ util/kelvinator/differenceFrames.py
@@ -1,9 +1,9 @@
 import sys
 import os
 
-from scipy.misc import imread
-from scipy.linalg import norm
-from scipy import sum, average
+import math
+import operator
+from PIL import Image
 
 def createManifest(keepList, keepIndicies, frameRate):
     outfile = "jpegs/manifest.txt"
@@ -62,37 +62,12 @@ def main():
     createManifest(keepList, keepIndicies, sys.argv[2])
     print keepList
 
+# from http://mail.python.org/pipermail/image-sig/1997-March/000223.html
 def computeDiff(file1, file2):
-    # read images as 2D arrays (convert to grayscale for simplicity)
-    img1 = to_grayscale(imread(file1).astype(float))
-    img2 = to_grayscale(imread(file2).astype(float))
-    # compare
-    n_m, n_0 = compare_images(img1, img2)
-    return n_m*1.0/img1.size
-
-def compare_images(img1, img2):
-    # normalize to compensate for exposure difference, this may be unnecessary
-    # consider disabling it
-    img1 = normalize(img1)
-    img2 = normalize(img2)
-    # calculate the difference and its norms
-    diff = img1 - img2  # elementwise for scipy arrays
-    m_norm = sum(abs(diff))  # Manhattan norm
-    z_norm = norm(diff.ravel(), 0)  # Zero norm
-    return (m_norm, z_norm)
-
-
-def to_grayscale(arr):
-    "If arr is a color image (3D array), convert it to grayscale (2D array)."
-    if len(arr.shape) == 3:
-        return average(arr, -1)  # average over the last axis (color channels)
-    else:
-        return arr
-
-def normalize(arr):
-    rng = arr.max()-arr.min()
-    amin = arr.min()
-    return (arr-amin)*255/rng
+    h1 = Image.open(file1).histogram()
+    h2 = Image.open(file2).histogram()
+    rms = math.sqrt(reduce(operator.add, map(lambda a,b: (a-b)**2, h1, h2))/len(h1))
+    return rms
 
 if __name__ == "__main__":
     main()
--- util/kelvinator/kelvinator.sh
+++ util/kelvinator/kelvinator.sh
@@ -41,16 +41,16 @@ working_dir="${working_path}/extract-${today_epoch}"
 mydir=`pwd`
 
 source_dir="."
-using_s3=false
+using_s3=0
 # if first character is a slash, then full path, extract it
 # otherwise assume it's right here.  
 if [[ ${1:0:5} == "s3://" || ${1:0:5} == "S3://" ]]; then
     source_dir=`dirname $1`
-    using_s3=true
+    using_s3=1
 fi
 video_file=`basename $1`
 
-if [[ $using_s3 ]]; then
+if [[ $using_s3 == 1 ]]; then
     echo "$me: setting up working directory: ${working_dir}"
     mkdir -p ${working_dir} 
     s3cmd get $1 ${working_dir}/$video_file
@@ -73,7 +73,7 @@ fi
 #Runs ffmpeg to extract frames at a certain interval
 echo
 echo "$me: Starting Extraction (ffmpeg)"
-ffmpeg -i $video_file -r $2 -f image2 jpegs/img%3d.jpeg 
+ffmpeg -i $video_file -r $2 -f image2 -s vga jpegs/img%3d.jpeg 
 
 #Runs extractFrames to list frames to be deleted
 echo
@@ -92,7 +92,7 @@ done < $myFile
 # if we are doing in place then we are done.  But if we are using S3 we need to 
 # upload files (and manifest) back up there
 
-if [[ $using_s3 ]]; then
+if [[ $using_s3 == 1 ]]; then
     echo "$me: copying frames to target: $source_dir/$frame_dir"
     s3cmd sync $frame_dir $source_dir/
 
