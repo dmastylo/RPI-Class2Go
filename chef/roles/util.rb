505779b7ca37b44d9dcacc96a7cd3974b78d370c
Sef Kloninger <sef@kloninger.com>
2012-08-28 17:57:24 -0700

    Add util machine role for kelvinator, other async things later (#468)

--- chef/roles/util.rb
+++ chef/roles/util.rb
@@ -1,20 +1,15 @@
-name "appserver"
-description "Class2Go appserver node"
+name "util"
+description "Class2Go utility node"
 
 override_attributes({
     "class2go-bitnami-django" => {
-            "django-app" => "main"
+            "django-app" => "util"
             }
 })
 
-
-# For now not doing an update/upgrade before everything else since it
-# can cause mysterious problems and takes so darn long.  have to do it 
-# on the util machines though
-
 run_list(
 #   "recipe[chef-client]",
-#   "recipe[class2go-update]",
+    "recipe[class2go-update]",
     "recipe[gdata]",
     "recipe[class2go-base]",
     "recipe[class2go-python]",
@@ -22,6 +17,7 @@ run_list(
     "recipe[class2go-deploy]",
     "recipe[class2go-logging]",
     "recipe[class2go-database.py]",
-    "recipe[class2go-collectstatic]",
+#    "recipe[class2go-collectstatic]",
+    "recipe[class2go-kelvinator]",
     "recipe[class2go-bitnami-apache-restart]"
 )
