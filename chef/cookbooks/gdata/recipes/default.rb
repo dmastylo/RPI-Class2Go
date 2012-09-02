7903563dae589331b22219b5efbe47de9e4fb10d
Sef Kloninger <sef@kloninger.com>
2012-09-01 22:50:42 -0700

    gdata recipe: parameterize user/group (#519)

--- chef/cookbooks/gdata/recipes/default.rb
+++ chef/cookbooks/gdata/recipes/default.rb
@@ -1,32 +1,32 @@
 cookbook_file "/tmp/gdata-2.0.17-c2g.tar.gz" do
   source "gdata-2.0.17-c2g.tar.gz"
-  owner "bitnami"
-  mode "0644"
+  owner node['system']['admin_user']
+  mode 00644
   action :create
 end
 
 execute "untar" do
-  user "bitnami"
+  user node['system']['admin_user']
   cwd "/tmp"
   command "tar zxf gdata-2.0.17-c2g.tar.gz"
   action :run
 end
 
 execute "install" do
-  user "bitnami"
+  user "root"
   cwd "/tmp/gdata-2.0.17"
-  command "sudo python setup.py install"
+  command "python setup.py install"
   action :run
 end
 
 execute "cleanup-dir" do
-    user "bitnami"
+    user node['system']['admin_user']
     command "rm -r /tmp/gdata-2.0.17"
     action :run
 end
 
 execute "cleanup-tarball" do
-    user "bitnami"
+    user node['system']['admin_user']
     command "rm /tmp/gdata-2.0.17-c2g.tar.gz"
     action :run
 end
---
+++ chef/cookbooks/sophi-base-bitnami/recipes/default.rb
@@ -0,0 +1,5 @@
+
+package "git" do
+    action :install
+end
+
