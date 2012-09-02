567575e776f63e9dea86defe63ede73fd8d4fa59
Sef Kloninger <sef@kloninger.com>
2012-09-02 10:13:11 -0700

    Remove old way of running celery (was just commented-out) (#519)

--- chef/cookbooks/sophi-celery-worker/recipes/default.rb
+++ chef/cookbooks/sophi-celery-worker/recipes/default.rb
@@ -10,30 +10,12 @@ execute "pip install django-celery" do
     action :run
 end
 
-execute "celery worker db setup" do
+execute "celery worker local database setup" do
     cwd node['system']['admin_home'] + "/sophi/main"
     user "root"
     command "./manage.py syncdb --migrate --noinput --settings=settings_util"
     action :run
 end
-
-# TODO: turn in to a proper service
-# - maybe use the chef "service" resource
-# - or consider using python-daemon module
-# - or maybe the celery command line?
-#
-# Maybe use this as an example? 
-#   http://stackoverflow.com/questions/9938314/chef-how-to-run-a-template-that-creates-a-init-d-script-before-the-service-is-c
-#
-# it appears that just trying to put in the background like this won't work, most 
-# likely because this isn't running in an interactive shell
-#
-# execute "celery worker start" do
-#    cwd node['system']['admin_home'] + "/sophi/main"
-#    user "root"
-#    command "./manage.py celery worker --loglevel=info --settings=settings_util &"
-#    action :run
-#end
 
 cookbook_file "celeryd init script" do
     source "celeryd-init-script"
