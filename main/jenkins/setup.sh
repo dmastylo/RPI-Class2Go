9b2867f8b36679b4bb231799944f8be3a814e670
Sef Kloninger <sef@kloninger.com>
2012-09-07 12:53:55 -0700

    OK to clobber database.py if it's my own (jenkins) one

--- main/jenkins/setup.sh
+++ main/jenkins/setup.sh
@@ -1,14 +1,35 @@
-#!/bin/bash -ex
+#!/bin/bash -x
 #
 # Assumes current working directory is root of repo (class2go)
+# Can't be run with "-e" flag since I'm using exit status of grep
 #
 
-# -n prevents clobber if already there 
-cp -n main/jenkins/database_ci.py main/database.py
+if [[ -r main/database.py ]]; then
+    echo "Checking to see if OK to clobber this database.py"
+    grep 'INSTANCE = "jenkins"' main/database.py
+    if [[ $? -eq 0 ]]; then
+        echo "Looks like it's mine, clobbering"
+        cp main/jenkins/database_ci.py main/database.py
+    else
+        echo "Hey, this isn't mine, failing"
+        exit 1
+    fi
+else
+    cp -n main/jenkins/database_ci.py main/database.py
+fi
 
 python main/manage.py syncdb
+if [[ $? -ne 0 ]]; then
+    exit $?
+fi
 
 python main/manage.py migrate
+if [[ $? -ne 0 ]]; then
+    exit $?
+fi
 
 python main/manage.py db_populate
+if [[ $? -ne 0 ]]; then
+    exit $?
+fi
 
