c37916aabceeb9485ba898ba7ca1bc76f5fee6a5
Sef Kloninger <sef@kloninger.com>
2012-09-12 00:00:04 -0700

    tabs to spaces; add cron jobs for reports

--- chef/cookbooks/class2go-reports/recipes/default.rb
+++ chef/cookbooks/class2go-reports/recipes/default.rb
@@ -14,3 +14,15 @@ template "mysql-client-config" do
     mode 00644
 end
 
+cron "users_by_class" do
+    hour "9"
+    minute "0"
+    command node['system']['admin_home'] + "/class2go/reports/users_by_class.sh olteam@cs.stanford.edu,c2g-dev@cs.stanford.edu"
+end
+
+cron "users_by_day" do
+    hour "9"
+    minute "10"
+    command node['system']['admin_home'] + "/class2go/reports/users_by_day.sh olteam@cs.stanford.edu,c2g-dev@cs.stanford.edu"
+end
+
