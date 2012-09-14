c7183e4eb3f732474036c364e2854b3c7e3610c4
Sef Kloninger <sef@kloninger.com>
2012-09-14 11:16:38 -0700

    Write s3 config file to daemon homedir also (#676)

--- chef/cookbooks/s3cmd/recipes/default.rb
+++ chef/cookbooks/s3cmd/recipes/default.rb
@@ -9,6 +9,16 @@ template "dot-s3cfg" do
     source "dot-s3cfg.erb"
     owner node['system']['admin_user']
     owner node['system']['admin_group']
-    mode 00644
+    mode 00600
+end
+
+# Write same config file to the root of the daemon user, since that's
+# the account that the kelvinator runs as on deployed util machines.
+template "dot-s3cfg" do
+    path node['system']['admin_home'] + "/.s3cfg"
+    source "dot-s3cfg.erb"
+    owner daemon
+    owner daemon
+    mode 00600
 end
 
