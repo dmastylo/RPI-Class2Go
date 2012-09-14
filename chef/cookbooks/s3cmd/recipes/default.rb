2a413e1bdfe8143632e80411c9b94599db572574
Sef Kloninger <sef@kloninger.com>
2012-09-14 11:49:14 -0700

    Move s3cmd config file to /opt/class2go (#676)
    Were having problems finding it.  Having it squirreled away under some
    users home dir is annoying.  So in prod now we store in
    /opt/class2go/s3cmd.conf.  In dev, if that file doesn't exist, still use
    your local one.

--- chef/cookbooks/s3cmd/recipes/default.rb
+++ chef/cookbooks/s3cmd/recipes/default.rb
@@ -3,27 +3,22 @@ package "s3cmd" do
     action :install
 end
 
-
-template "dot-s3cfg" do
-    path node['system']['admin_home'] + "/.s3cfg"
-    source "dot-s3cfg.erb"
+# the recipe class2go-deploy also creates this directory today, so
+# this is redundant.  But don't like creating that dependency, 
+# especially since we may not want that code to live in "deploy"
+# long term.
+directory "/opt/class2go" do
     owner node['system']['admin_user']
-    owner node['system']['admin_group']
-    mode 00600
+    group node['system']['admin_group']
+    mode 00777
+    action :create
 end
 
-# Write same config file daemon user's home dir (/usr/sbin) (!), since that's
-# the account that the kelvinator runs as on deployed util machines.
-#
-# TODO: once @halawa's changes are done, consider putting outside homedirs
-# and referencing explicitly with the --config option to s3cmd.  Or just 
-# start using the s3 API properly.
-
-template "dot-s3cfg" do
-    path "/usr/sbin/.s3cfg"
-    source "dot-s3cfg.erb"
-    owner "daemon"
-    owner "daemon"
-    mode 00600
+template "s3cfg.conf" do
+    path "/opt/class2go/s3cfg.conf"
+    source "s3cfg_conf.erb"
+    owner node['system']['admin_user']
+    owner node['system']['admin_group']
+    mode 00644
 end
 
