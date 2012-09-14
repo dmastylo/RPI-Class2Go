ddebce4144a862569a07b08606f5602a799a4ff0
Sef Kloninger <sef@kloninger.com>
2012-09-14 11:26:14 -0700

    Write config to proper location, /usr/sbin (#676)

--- chef/cookbooks/s3cmd/recipes/default.rb
+++ chef/cookbooks/s3cmd/recipes/default.rb
@@ -12,10 +12,15 @@ template "dot-s3cfg" do
     mode 00600
 end
 
-# Write same config file to the root of the daemon user, since that's
+# Write same config file daemon user's home dir (/usr/sbin) (!), since that's
 # the account that the kelvinator runs as on deployed util machines.
+#
+# TODO: once @halawa's changes are done, consider putting outside homedirs
+# and referencing explicitly with the --config option to s3cmd.  Or just 
+# start using the s3 API properly.
+
 template "dot-s3cfg" do
-    path node['system']['admin_home'] + "/.s3cfg"
+    path "/usr/sbin/.s3cfg"
     source "dot-s3cfg.erb"
     owner "daemon"
     owner "daemon"
