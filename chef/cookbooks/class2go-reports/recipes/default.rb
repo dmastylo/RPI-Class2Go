069912dd1c5d08edb8b981d597c57bb9e07fc6f4
Sef Kloninger <sef@kloninger.com>
2012-09-18 21:52:57 -0700

    Add recipients to nightly user reports
    Eventually this will be replaced by a mailing list, but haven't created
    it yet, so for now just add Nick, Phil, and Bruce manually.

--- chef/cookbooks/class2go-reports/recipes/default.rb
+++ chef/cookbooks/class2go-reports/recipes/default.rb
@@ -22,13 +22,13 @@ cron "users_by_class" do
     hour "10"   # GMT
     minute "0"
     user node['system']['admin_user']
-    command "(cd #{node['system']['admin_home']}/class2go/reports; ./users_by_class.sh olteam@cs.stanford.edu,c2g-dev@cs.stanford.edu)"
+    command "(cd #{node['system']['admin_home']}/class2go/reports; ./users_by_class.sh olteam@cs.stanford.edu,c2g-dev@cs.stanford.edu,nickm@stanford.edu,pal@cs.stanford.edu,bmc@stanford.edu)"
 end
 
 cron "users_by_day" do
     hour "10"   # GMT
     minute "10"
     user node['system']['admin_user']
-    command "(cd #{node['system']['admin_home']}/class2go/reports; ./users_by_day.sh olteam@cs.stanford.edu,c2g-dev@cs.stanford.edu)"
+    command "(cd #{node['system']['admin_home']}/class2go/reports; ./users_by_day.sh olteam@cs.stanford.edu,c2g-dev@cs.stanford.edu,nickm@stanford.edu,pal@cs.stanford.edu,bmc@stanford.edu)"
 end
 
