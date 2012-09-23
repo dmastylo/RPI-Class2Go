284cd14b9a6af8a803ac61ecaa83699e4610eaba
Sef Kloninger <sef@kloninger.com>
2012-09-22 19:33:07 -0700

    Add comment - consider "INFO" for settings.py (#689)
    ./manage.py kelvinator is better with info level logging to console, I
    wonder if this is OK for other commands?  What else goes to console?

--- main/settings.py
+++ main/settings.py
@@ -259,6 +259,7 @@ LOGGING = {
             'backupCount': 3,
         },
         'console':{
+            # consider "INFO" for better manage.py interactive commands
             'level':'WARNING',
             'class':'logging.StreamHandler',
             'formatter': 'simple'
