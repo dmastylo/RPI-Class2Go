3ead2ae36caafc0d40059d6512bd4855b182523b
Sherif Halawa <halawa@stanford.edu>
2012-09-23 13:48:01 -0700

    Makes courses.reports.views.main use the common_page_data middleware for better exception handling

--- main/courses/reports/views.py
+++ main/courses/reports/views.py
@@ -7,10 +7,5 @@ from courses.actions import auth_is_course_admin_view_wrapper
 
 @auth_is_course_admin_view_wrapper
 def main(request, course_prefix, course_suffix):
-    try:
-        common_page_data = get_common_page_data(request, course_prefix, course_suffix)
-    except:
-        raise Http404
-        
-    dashboard_data = get_dashboard_data(common_page_data['draft_course'])
-    return render_to_response('reports/main.html', {'common_page_data':common_page_data, 'dd':dashboard_data}, context_instance=RequestContext(request))
+    dashboard_data = get_dashboard_data(request.common_page_data['draft_course'])
+    return render_to_response('reports/main.html', {'common_page_data':request.common_page_data, 'dd':dashboard_data}, context_instance=RequestContext(request))
