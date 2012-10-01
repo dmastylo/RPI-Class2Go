7c52b696e14867699c1e427653f9685e46056fe6
halawa <halawa@stanford.edu>
2012-10-01 16:52:09 -0700

    Reset courses/views.py to its production state

--- main/courses/views.py
+++ main/courses/views.py
@@ -69,10 +69,7 @@ def main(request, course_prefix, course_suffix):
     file_list = File.objects.getByCourse(course=common_page_data['course'])
 
     full_index_list = []
-    full_contentsection_list=[]
-    
     for contentsection in contentsection_list:
-    
         index_list = []
         for video in video_list:
             if video.section.id == contentsection.id:
@@ -99,26 +96,20 @@ def main(request, course_prefix, course_suffix):
                     icon_type="picture"  
                 elif (file_extension in ('mp3', 'aac')):
                     icon_type="music"
-                elif (file_extension in ('gz', 'zip', 'tar', 'bz', 'bz2')):
-                    icon_type="download-alt"
                 else:
                     icon_type="file"
                 index_list.append(('file', file.index, file.id, contentsection.id, file.file.url, file.title, icon_type))
 
         index_list.sort(key = index)
-        
         full_index_list.append(index_list)
 
-       #don't show empty sections
-        if index_list:
-            full_contentsection_list.append(contentsection)
 
     return render_to_response('courses/view.html',
             {'common_page_data': common_page_data,
              'announcement_list': announcement_list,
              'many_announcements':many_announcements,
              'news_list': news_list,
-             'contentsection_list': full_contentsection_list,
+             'contentsection_list': contentsection_list,
              'video_list': video_list,
              'pset_list': pset_list,
              'full_index_list': full_index_list,
