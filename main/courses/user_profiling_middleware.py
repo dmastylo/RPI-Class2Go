7591670b374d67ebbca6c5873d77aa8187db39a3
Sef Kloninger <sef@kloninger.com>
2012-10-06 17:30:25 -0700

    Only update client info once per day (#985)
    Prevent performance problems that could come from turning every
    read into a write.  Likely source of contention.

--- main/courses/user_profiling_middleware.py
+++ main/courses/user_profiling_middleware.py
@@ -1,18 +1,37 @@
+
 from django.contrib.auth.models import User
+from django.conf import settings
 from c2g.models import UserProfile
+
+from datetime import datetime
 
 import logging
 logger = logging.getLogger(__name__)
 
+
 class user_profiling(object):
     """
-    Middleware to keep track of the stuff about the user that we want to keep track of.  
-    The results end up in the UserProfile model.
+    Middleware to keep track of useful stuff about the user.  Only update once 
+    per day (overrideable).  The results end up in the UserProfile model.
+
+    Note that we use the last_update timestamp for this, which will be updated
+    by *anything* writing to the UserProfile model.  So if there is something
+    else writing to this table frequently then we will stop updating this client
+    information.  Fix for this would be to track a separate timestamp for client
+    info tracking time -- that feels like overkill for now.
     """
     def process_view (self, request, view_func, view_args, view_kwargs):
         if request.user.is_authenticated():
             try:
                 profile = request.user.get_profile()
+
+                # don't update client data too frequently, bad for performance
+                # by defaut, once per day
+                resolution = getattr(settings, "PROFILE_UPDATE_RESOLUTION", 86400)
+                delta = datetime.now()-profile.last_updated
+                if delta.total_seconds() < resolution:
+                    return
+
                 try:
                     profile.client_ip = request.META['X-FORWARDED-FOR']
                 except KeyError:
@@ -49,4 +68,3 @@ class user_profiling(object):
                 # that's OK, don't do any tracking for those
                 pass
 
-        
