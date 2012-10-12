f991e65239a1ccf74876d65c4798bf07ed6b19ea
Jason Bau <jbau@stanford.edu>
2012-10-12 11:06:40 -0700

    more consistency in variable assignment

--- main/exception_snippet/midware.py
+++ main/exception_snippet/midware.py
@@ -14,10 +14,11 @@ class error_ping(object):
             return None
         username = request.user.username if request.user.is_authenticated() else "Anonymous"
         datestring = datetime.datetime.isoformat(datetime.datetime.now())
+        user_agent = request.META['HTTP_USER_AGENT']
         (type, value, tb) = sys.exc_info()
         (path, lineno, exc, text) = traceback.extract_tb(tb)[-1]
         email_subj = "%s on %s" % (repr(exception), repr(socket.gethostname()))
-        email_msg = "User: %s\nTime: %s\nUser-agent: %s\nFile path %s:%d\nLine text: %s\n" % (username, datestring, request.META['HTTP_USER_AGENT'], path, lineno,  text)
+        email_msg = "User: %s\nTime: %s\nUser-agent: %s\nFile path %s:%d\nLine text: %s\n" % (username, datestring, user_agent, path, lineno,  text)
         send_mail(email_subj, email_msg, from_addr, mailto_list)
         del tb
         return None
