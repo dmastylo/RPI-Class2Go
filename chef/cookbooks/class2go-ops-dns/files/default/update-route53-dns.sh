ea33ed1f0d68c29c735dd1a7df2fc3f78eb18b92
Sef Kloninger <sef@kloninger.com>
2012-10-22 16:54:47 +0000

    Get upstart working for update-route53 (#643)
    - upstart conf needs a exec
    - look directly at /etc/hostname instead of the `hostname` command to
      remove dependency
    - have to wedge bitnami config into the shell script (yuck)

--- chef/cookbooks/class2go-ops-dns/files/default/update-route53-dns.sh
+++ chef/cookbooks/class2go-ops-dns/files/default/update-route53-dns.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 # Make sure only root can run our script
 if [ "$(id -u)" != "0" ]; then
 echo "This script must be run as root" 1>&2
     exit 1
 fi
+
+# Need to have environment right for bitnami
+. /opt/bitnami/scripts/setenv.sh
 
 # Load configuration
 . /etc/route53/config
@@ -14,7 +17,7 @@ export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
 
 # Use command line scripts to get hostname and public hostname
-HOSTNAME_FULL=`hostname`
+HOSTNAME_FULL=`cat /etc/hostname`
 HOSTNAME_PART=${HOSTNAME_FULL%%.$ZONE}
 PUBLIC_HOSTNAME=$(ec2metadata | grep 'public-hostname:' | cut -d ' ' -f 2)
 
