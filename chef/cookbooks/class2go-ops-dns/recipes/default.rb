8eb6c26e894a66f7e704dbb401df54e9035348f2
Sef Kloninger <sef@kloninger.com>
2012-10-23 14:16:07 -0700

    Distribute cli53 utility ourselves (#643)
    Don't like being dependent on lots of third-party repos.  One fewer,
    yay.

--- chef/cookbooks/class2go-ops-dns/recipes/default.rb
+++ chef/cookbooks/class2go-ops-dns/recipes/default.rb
@@ -8,11 +8,14 @@ easy_install_package "dnspython" do
     action :install
 end
 
-execute "cli53" do
-    user "root"
-    creates "/opt/bitnami/python/bin/cli53"
-    command "easy_install https://github.com/barnybug/cli53/tarball/master" 
-    action :run
+# cli53 -- command line util to manage settings, just a wrapper around boto.
+# instead of being dependendent on this guy's repo, just copy and distribute
+# the utility straight from here. see: https://github.com/barnybug/cli53
+cookbook_file "/usr/locao/bin/cli53" do
+    owner "root"
+    group "root"
+    mode 00755
+    action :create
 end
 
 directory "/etc/route53" do
