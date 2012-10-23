b9448743b4757d74dcb83c23955d273ca59fe956
dcadams <dcadams@.stanford.edu>
2012-10-23 13:53:20 -0700

    Setup for test.

--- main/courses/course_materials.py
+++ main/courses/course_materials.py
@@ -25,6 +25,8 @@ def get_course_materials(common_page_data, get_video_content=False, get_pset_con
 
         if get_pset_content:
             
+            test_for_pset_progress_and_score()
+            
             problem_sets = ProblemSet.objects.getByCourse(course=common_page_data['course'])
             if problem_sets:
                 problem_set_list = []
@@ -328,7 +330,7 @@ def get_course_materials(common_page_data, get_video_content=False, get_pset_con
 #Test purposes only - not to be run in production
 def test_for_pset_progress_and_score():
     
-    logfile = open('zzzz_1.log', 'w')
+    logfile = open('zzzz.log', 'w')
     
     #Get all courses
     courses = Course.objects.filter(mode='ready')
