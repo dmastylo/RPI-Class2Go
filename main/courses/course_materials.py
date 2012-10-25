0f8fa4b24b9a5106dfcf651e36de9bb05c3ca52a
dcadams <dcadams@.stanford.edu>
2012-10-25 11:55:11 -0700

    #1180; problemsets not showing up in edit mode.

--- main/courses/course_materials.py
+++ main/courses/course_materials.py
@@ -249,6 +249,7 @@ def get_course_materials(common_page_data, get_video_content=False, get_pset_con
                                 break                            
                             
                         if common_page_data['course_mode'] == 'draft':
+                            print "in draft mode"
                             prod_problem_set = problem_set.image
                             if not prod_problem_set.live_datetime:
                                 visible_status = "<span style='color:#A00000;'>Not Live</span>"
@@ -262,7 +263,7 @@ def get_course_materials(common_page_data, get_video_content=False, get_pset_con
                                     visible_status = "<span style='color:#A07000;'>Live %02d-%02d-%04d at %02d:%02d</span>" % (month,day,year,hour,minute)
                                 else:
                                     visible_status = "<span style='color:green;'>Live</span>"
-
+                            print "visible status : " + str(visible_status)
                             item['visible_status'] = visible_status
                         else:
                                         
@@ -322,7 +323,7 @@ def get_course_materials(common_page_data, get_video_content=False, get_pset_con
                             item['progress'] = progress
 
                             item['numQuestions'] = numQuestions
-                            section_dict['items'].append(item)
+                        section_dict['items'].append(item)
 
             if common_page_data['course_mode'] == 'draft' or len(section_dict['items']) > 0:
                 section_dict['items'] = sorted(section_dict['items'], key=lambda k: k['index'])
