4538185fbbb45ea6800c5976bbf4e074054c32c8
Sef Kloninger <sef@kloninger.com>
2012-11-03 09:25:10 -0700

    Separate kelvinator and resize tasks onto "long" queue (#1135)
    Use celery router to put long-running tasks on a separate queue.  So now
    we will see "prod-celery" and "prod-long" queue's in SQS.
    
    Configure workers so instead of one worker named just "w1" now we
    will have "default" and "long" workers.  Concurrency on the long
    worker is always 1, and concurrency on the default one can be much
    higher, say 10.

--- main/celerytest/tasks.py
+++ main/celerytest/tasks.py
@@ -1,5 +1,18 @@
+import time
 from celery import task
 
 @task()
 def add(x, y):
     return x + y
+
+@task()
+def echo(v, t):
+    """return value v after sleeping for t seconds"""
+    time.sleep(t)
+    return v
+
+@task()
+def echo_long(v, t):
+    """return value v after sleeping for t seconds"""
+    time.sleep(t)
+    return v
