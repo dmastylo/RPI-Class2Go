68cd4a62ebf24265e280491067a8f5254d4bd01a
Sef Kloninger <sef@kloninger.com>
2012-11-05 14:23:15 -0800

    whoops missing request context (#1230)

--- main/courses/landing/views.py
+++ main/courses/landing/views.py
@@ -6,6 +6,7 @@ def landing(request):
     """For normal servers, return our project landing page.  For maint servers,
     show our maintenance page."""
 
+    context = RequestContext(request)
     maint_override = getattr(settings, 'MAINTENANCE_LANDING_PAGE', False)
     if maint_override:
         r = render_to_response('maint.html', context_instance=context)
