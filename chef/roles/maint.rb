bb64be317273313f1103caee7f0fe5878b54d2cc
Sef Kloninger <sef@kloninger.com>
2012-11-05 22:25:52 +0000

    Turns out we do rely on gdata, adding back to maint roles defn (#1230)

--- chef/roles/maint.rb
+++ chef/roles/maint.rb
@@ -13,11 +13,14 @@ override_attributes \
 
 run_list(
     "recipe[class2go-apt-update]",
+    "recipe[gdata]",
     "recipe[class2go-base-ubuntu]",
     "recipe[class2go-apache]",
     "recipe[class2go-python]",
     "recipe[class2go-deploy]",
     "recipe[class2go-logging]",
     "recipe[class2go-ops-dns]",
-    "recipe[class2go-database-config]"
+    "recipe[class2go-database-config]",
+    "recipe[class2go-collectstatic]",
+    "recipe[class2go-apache-restart]"
 )
