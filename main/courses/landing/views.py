34a96a31a598492832c54e311c2ff4e8ff8f5fae
Sef Kloninger <sef@kloninger.com>
2012-11-05 11:15:37 -0800

    Maint page support (#1230)
    Current maint page message is hardcoded for today's maint, in the
    future we might want to make that more useful.

--- main/courses/landing/views.py
+++ main/courses/landing/views.py
@@ -1,8 +1,14 @@
-
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.conf import settings
 
 def landing(request):
-    context = RequestContext(request)
-    return render_to_response('landing/landing.html', context_instance=context)
+    """For normal servers, return our project landing page.  For maint servers,
+    show our maintenance page."""
 
+    maint_override = getattr(settings, 'MAINTENANCE_LANDING_PAGE', False)
+    if maint_override:
+        r = render_to_response('maint.html', context_instance=context)
+    else:
+        r = render_to_response('landing/landing.html', context_instance=context)
+    return r
