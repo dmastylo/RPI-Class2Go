87d87924d1e28d7019a5a030cf85cb531183b461
Sef Kloninger <sef@kloninger.com>
2012-11-07 16:19:38 -0800

    Revert "Improve asset link generation for 'local' sites"
    This reverts commit b7c338e95ac54b5ad3a91e977c83f9c92629833b.

--- main/db_scripts/management/commands/update_site.py
+++ main/db_scripts/management/commands/update_site.py
@@ -1,5 +1,3 @@
-import sys
-
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.sites.models import Site
 
@@ -8,23 +6,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        domainname = 'class.stanford.edu'
-        if len(args) == 1:
-            domainname = args[0]
-        elif len(args) > 1:
-            print "Error; I don't know what to do with so many arguments"
-            sys.exit(1)
-
         try:
             site = Site.objects.get(id=1)
-            #site.domain = 'class.stanford.edu'
-            site.domain = domainname
+            site.domain = 'class.stanford.edu'
             site.name = 'Stanford Online'
             site.save()
             print "Site updated successfully."
         except:
             print "Error; the site record was not found with id = 1"
-            sys.exit(1)
 
-        sys.exit(0)
 
