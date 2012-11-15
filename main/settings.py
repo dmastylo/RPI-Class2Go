84ad34f2a5205ad5528a289fa9d5cb4100fae286
John Gaunt <redfive@jgaunt.com>
2012-11-15 00:59:20 -0800

    Removing a temp. comment

--- main/settings.py
+++ main/settings.py
@@ -196,9 +196,7 @@ if INSTANCE != "prod":
                         'django_nose',
                        )
 
-# XXXredfive - trying out the django_nose stuff
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
