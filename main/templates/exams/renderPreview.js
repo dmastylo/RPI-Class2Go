921f9deea73cf83cc4a233c07273ce9700133582
Mike Wescott <michael.wescott@gmail.com>
2012-11-14 16:28:32 -0800

    Fix form saving button 	modified:   create_exam.html 	modified:   renderPreview.js

--- main/templates/exams/renderPreview.js
+++ main/templates/exams/renderPreview.js
@@ -1,10 +1,11 @@
 var c2gXMLParse = (function() {
    
-   var c2gXMLParse = {
+    var c2gXMLParse = {
        
-     renderPreview: function() {
+    renderPreview: function() {
     
         $('#staging-area').empty();
+
         // Set up XML DOM
         var sourceXML = editor.getValue();
         try {
@@ -14,9 +15,13 @@ var c2gXMLParse = (function() {
             console.log(e.message);
             return;
         }
+
         var problemNodes = $(myDOM).find('problem');
+
         problemNodes.each(function () {
+
             questionIdx += 1;
+
             var tmpProbDiv = document.createElement('div');
             $(tmpProbDiv).addClass('question');
             
@@ -26,29 +31,31 @@ var c2gXMLParse = (function() {
             
             var renderResponseNode = function (node, arg2, idx_suffix) {
             
-              var nodeName = $(node)[0].nodeName;
-              var nodeParent = arguments[1] || $(tmpProbDiv);
+            var nodeName = $(node)[0].nodeName;
+            var nodeParent = arguments[1] || $(tmpProbDiv);
             
-              switch (nodeName) {
+            switch (nodeName) {
                 case 'multiplechoiceresponse':
                     var choices = $($(node)).find('choice');
             
                     $(choices).each(function (idx, el) {
-                            var tmpInput = document.createElement('input');
-                            var tmpLabel = document.createElement('label');
-                            $(tmpLabel).attr('for', 'q' + questionIdx + '_' + idx);
+                        var tmpInput = document.createElement('input');
+                        var tmpLabel = document.createElement('label');
+                        $(tmpLabel).attr('for', 'q' + questionIdx + '_' + idx);
                             
-                            $(tmpInput).attr('type', 'radio');
-                            $(tmpInput).attr('id', 'q' + questionIdx + '_' + idx);
-                            $(tmpInput).attr('name', 'q' + questionIdx);
-                            $(tmpInput).attr('value', $(this).attr('name'));
+                        $(tmpInput).attr('type', 'radio');
+                        $(tmpInput).attr('id', 'q' + questionIdx + '_' + idx);
+                        $(tmpInput).attr('name', 'q' + questionIdx);
+                        $(tmpInput).attr('value', $(this).attr('name'));
                             
-                            $(tmpLabel).text($(this).text());
-                            
-                            $(nodeParent).append($(tmpLabel));
-                            $(tmpLabel).append($(tmpInput));
+                        $(tmpLabel).text($(this).text());
+                        
+                        $(nodeParent).append($(tmpLabel));
+                        $(tmpLabel).append($(tmpInput));
                     });
+
                     break;
+
                 case 'stringresponse':
                 case 'numericalresponse':
                     
@@ -63,20 +70,40 @@ var c2gXMLParse = (function() {
                     $(tmpInput).attr('size', textInputSize);
             
                     $(nodeParent).append($(tmpInput));
+
                     break;
+
                 case 'optionresponse':
+
                     var optionData = $(node).find('optioninput');
                     var optionItemStr = $(optionData).attr('options');
+                    var optionItemArr = optionItemStr.split(',');
+
+                    var tmpSelect = document.createElement('select');
+                    for (var i = 0; i < optionItemArr.length; i += 1) {
+                        var tmpOptionItem = document.createElement('option');
+                        $(tmpOptionItem).text(optionItemArr[i]);
+                        $(tmpSelect).append($(tmpOptionItem));
+                    }
+
+                    $(nodeParent).append($(tmpSelect));
+
                     break;
+
                 default:
+
                     break;
+
               }
             
               if ($(nodeParent)[0] != $(tmpProbDiv)[0]) {
                     $(tmpProbDiv).append($(nodeParent));
               }
+
             };
+
             suffix_idx = 64
+
             $(allChildren).each(function () {
                 suffix_idx += 1;
                 var nodeName = $(this)[0].nodeName;
@@ -104,7 +131,8 @@ var c2gXMLParse = (function() {
         dataToTransmit.htmlContent = $('#staging-area').html();
         console.log(JSON.stringify(dataToTransmit));
       } // end renderpreview()
-    } // end c2gXMLParse
+    } // end c2gXMLParse object
+
     return c2gXMLParse;
    
 })();
