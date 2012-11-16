642b86bb5cff5befde140c860d7967a1e6b2d619
John Gaunt <redfive@jgaunt.com>
2012-11-16 15:25:01 -0800

    reverse the syntax

--- main/jenkins/db_basic.sh
+++ main/jenkins/db_basic.sh
@@ -5,7 +5,7 @@
 
 dbname=${C2G_JENKINS_DBNAME:-"class2go"}
 
-mysql --batch -e "drop database ${dbname} if exists; create database ${dbname} default character set 'utf8' default collate 'utf8_unicode_ci';"
+mysql --batch -e "drop database if exists ${dbname}; create database ${dbname} default character set 'utf8' default collate 'utf8_unicode_ci';"
 
 python main/manage.py syncdb --noinput
 
