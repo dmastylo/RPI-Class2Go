e480875ea9aa9d585489f0d81b363f6f7b93c920
John Gaunt <redfive@jgaunt.com>
2012-11-16 15:16:56 -0800

    Add a line to drop and add the database.

--- main/jenkins/db_basic.sh
+++ main/jenkins/db_basic.sh
@@ -3,6 +3,10 @@
 # This is meant to run as -e so failures are real
 #
 
+dbname=${C2G_JENKINS_DBNAME:-"class2go"}
+
+mysql --batch -e "drop database ${dbname}; create database ${dbname} default character set 'utf8' default collate 'utf8_unicode_ci';"
+
 python main/manage.py syncdb --noinput
 
 python main/manage.py migrate --noinput
