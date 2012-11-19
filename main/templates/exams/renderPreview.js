a4aa9a41789e0d6396366623da293992c1c87f2e
Mike Wescott <michael.wescott@gmail.com>
2012-11-19 09:28:09 -0800

    Commit to deal with some non-stashable changes 	modified:   main/templates/exams/create_exam.html 	modified:   main/templates/exams/renderPreview.js

--- main/templates/exams/renderPreview.js
+++ main/templates/exams/renderPreview.js
@@ -2,18 +2,43 @@ var c2gXMLParse = (function() {
    
     var c2gXMLParse = {
        
-    renderPreview: function() {
-    
-        $('#staging-area').empty();
+    renderHTML: function(xmlSourceEl, htmlOutputEl) {
+        $(htmlOutputEl).empty();
 
-        // Set up XML DOM
-        var sourceXML = editor.getValue();
         try {
-            var myDOM = $.parseXML(sourceXML);
+            var myDOM = $.parseXML(xmlSourceEl.val());
         } catch (e) {
             alert('Your XML is invalid');
             console.log(e.message);
             return;
+        
+    },
+
+    renderPreview: function(inputType) {
+    
+        if (inputType.toLowerCase() == "xml") {
+            editor.setValue('');
+            // Set up XML DOM
+            var sourceXML = $('#xml-entry'); 
+            try {
+                var myDOM = $.parseXML(sourceXML);
+            } catch (e) {
+                alert('Your XML is invalid');
+                console.log(e.message);
+                return;
+            }
+        } else {
+            $('#staging-area').empty();
+
+            // Set up XML DOM
+            var sourceXML = editor.getValue();
+            try {
+                var myDOM = $.parseXML(sourceXML);
+            } catch (e) {
+                alert('Your XML is invalid');
+                console.log(e.message);
+                return;
+            }
         }
 
         var problemNodes = $(myDOM).find('problem');
@@ -31,81 +56,82 @@ var c2gXMLParse = (function() {
             
             var renderResponseNode = function (node, arg2, idx_suffix) {
             
-            var nodeName = $(node)[0].nodeName;
-            var nodeParent = arguments[1] || $(tmpProbDiv);
-            
-            switch (nodeName) {
-                case 'multiplechoiceresponse':
-                    var choices = $($(node)).find('choice');
-                    var correctchoices = $(choices).filter(function() {
-                                                              if (!$(this).attr('correct'))
-                                                                 return false;
-                                                              return $(this).attr('correct').toLowerCase()==='true';
-                                                           });
-                    console.log(correctchoices.length + " correct choice");
-                    var inputtype = (correctchoices.length == 1)?'radio':'checkbox';
-                    
-                    $(choices).each(function (idx, el) {
-                        var tmpInput = document.createElement('input');
-                        var tmpLabel = document.createElement('label');
-                        $(tmpLabel).attr('for', 'q' + questionIdx + '_' + idx);
+                var nodeName = $(node)[0].nodeName;
+                var nodeParent = arguments[1] || $(tmpProbDiv);
+                
+                switch (nodeName) {
+                    case 'multiplechoiceresponse':
+
+                        var choices = $($(node)).find('choice');
+                        var correctchoices = $(choices).filter(function() {
+                                                                  if (!$(this).attr('correct'))
+                                                                     return false;
+                                                                  return $(this).attr('correct').toLowerCase()==='true';
+                                                               });
+                        console.log(correctchoices.length + " correct choice");
+                        var inputtype = (correctchoices.length == 1)?'radio':'checkbox';
+                
+                        $(choices).each(function (idx, el) {
+                            var tmpInput = document.createElement('input');
+                            var tmpLabel = document.createElement('label');
+                            $(tmpLabel).attr('for', 'q' + questionIdx + '_' + idx);
+                                
+                            $(tmpInput).attr('type', inputtype);
+                            $(tmpInput).attr('id', 'q' + questionIdx + '_' + idx);
+                            $(tmpInput).attr('name', 'q' + questionIdx);
+                            $(tmpInput).attr('value', $(this).attr('name'));
+                                
+                            $(tmpLabel).text($(this).text());
                             
-                        $(tmpInput).attr('type', inputtype);
-                        $(tmpInput).attr('id', 'q' + questionIdx + '_' + idx);
-                        $(tmpInput).attr('name', 'q' + questionIdx);
-                        $(tmpInput).attr('value', $(this).attr('name'));
-                            
-                        $(tmpLabel).text($(this).text());
+                            $(nodeParent).append($(tmpLabel));
+                            $(tmpLabel).append($(tmpInput));
+                        });
+
+                        break;
+
+                    case 'stringresponse':
+                    case 'numericalresponse':
                         
-                        $(nodeParent).append($(tmpLabel));
-                        $(tmpLabel).append($(tmpInput));
-                    });
+                        var textBoxData = $(node).children();   // only goes down one-level
+                
+                        var tmpInput = document.createElement('input');
+                        $(tmpInput).attr('type', 'text');
+                        $(tmpInput).attr('id', 'q' + questionIdx + idx_suffix);
+                        $(tmpInput).attr('name', 'q' + questionIdx + idx_suffix);
+                
+                        var textInputSize = (false) ? '' : 20;
+                        $(tmpInput).attr('size', textInputSize);
+                
+                        $(nodeParent).append($(tmpInput));
 
-                    break;
+                        break;
 
-                case 'stringresponse':
-                case 'numericalresponse':
-                    
-                    var textBoxData = $(node).children();   // only goes down one-level
-            
-                    var tmpInput = document.createElement('input');
-                    $(tmpInput).attr('type', 'text');
-                    $(tmpInput).attr('id', 'q' + questionIdx + idx_suffix);
-                    $(tmpInput).attr('name', 'q' + questionIdx + idx_suffix);
-            
-                    var textInputSize = (false) ? '' : 20;
-                    $(tmpInput).attr('size', textInputSize);
-            
-                    $(nodeParent).append($(tmpInput));
+                    case 'optionresponse':
 
-                    break;
+                        var optionData = $(node).find('optioninput');
+                        var optionItemStr = $(optionData).attr('options');
+                        var optionItemArr = optionItemStr.split(',');
 
-                case 'optionresponse':
+                        var tmpSelect = document.createElement('select');
+                        for (var i = 0; i < optionItemArr.length; i += 1) {
+                            var tmpOptionItem = document.createElement('option');
+                            $(tmpOptionItem).text(optionItemArr[i]);
+                            $(tmpSelect).append($(tmpOptionItem));
+                        }
 
-                    var optionData = $(node).find('optioninput');
-                    var optionItemStr = $(optionData).attr('options');
-                    var optionItemArr = optionItemStr.split(',');
+                        $(nodeParent).append($(tmpSelect));
 
-                    var tmpSelect = document.createElement('select');
-                    for (var i = 0; i < optionItemArr.length; i += 1) {
-                        var tmpOptionItem = document.createElement('option');
-                        $(tmpOptionItem).text(optionItemArr[i]);
-                        $(tmpSelect).append($(tmpOptionItem));
-                    }
+                        break;
 
-                    $(nodeParent).append($(tmpSelect));
+                    default:
 
-                    break;
+                        break;
 
-                default:
-
-                    break;
-
-              }
-            
-              if ($(nodeParent)[0] != $(tmpProbDiv)[0]) {
+                  }
+                
+                if ($(nodeParent)[0] != $(tmpProbDiv)[0]) {
                     $(tmpProbDiv).append($(nodeParent));
-              }
+                }
 
             };
 
