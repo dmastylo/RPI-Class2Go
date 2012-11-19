a785d23e7a9276bec0e78a5c3ff18ea4b27cb0aa
John Gaunt <redfive@jgaunt.com>
2012-11-19 11:58:23 -0800

    add checks for prod string in hostname and databases

--- main/jenkins/db_basic.sh
+++ main/jenkins/db_basic.sh
@@ -3,9 +3,26 @@
 # This is meant to run as -e so failures are real
 #
 
-dbname=${C2G_JENKINS_DBNAME:-"class2go"}
+JENKINS_DB_NAME=${C2G_JENKINS_DBNAME:-"class2go"}
+HOSTNAME=`hostname`
+INVALID_HOSTNAME="prod"
 
-mysql --batch -e "drop database if exists ${dbname}; create database ${dbname} default character set 'utf8' default collate 'utf8_unicode_ci';"
+if [[ $HOSTNAME =~ ${INVALID_HOSTNAME} ]]; then
+    echo "This script is so dangerous I won't let you run it on any machine"
+    echo "that has \"${INVALID_HOSTNAME}\" in the hostname. Sorry."
+    exit 1
+fi
+
+DATABASES=`mysql --batch -e "show databases;"`
+if [[ $DATABASES =~ $INVALID_HOSTNAME ]]; then
+    echo 
+    echo "Error: I see you have a database containing the name \"${INVALID_HOSTNAME}\" in it."
+    echo "Stopping since this might be an indication of a production database."
+    echo "Bailing out"
+    exit 1
+fi
+
+mysql --batch -e "drop database if exists ${JENKINS_DB_NAME}; create database ${JENKINS_DB_NAME} default character set 'utf8' default collate 'utf8_unicode_ci';"
 
 python main/manage.py syncdb --noinput
 
