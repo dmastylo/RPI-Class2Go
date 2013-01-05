5a93d06e86d46d2d54c398d5d1dd6b84492f14cd
Jason Bau <jbau@stanford.edu>
2013-01-04 18:27:06 -0800

    Starting file dialog

--- main/courses/files/views.py
+++ main/courses/files/views.py
@@ -1,20 +1,34 @@
 from django.http import Http404
 from django.shortcuts import render
 from courses.common_page_data import get_common_page_data
-
+from c2g.models import File
 from courses.files.forms import *
 from courses.actions import auth_is_course_admin_view_wrapper
 
 @auth_is_course_admin_view_wrapper
 def upload(request, course_prefix, course_suffix):
-    try:
-        common_page_data = get_common_page_data(request, course_prefix, course_suffix)
-    except:
-        raise Http404
 
+    common_page_data = request.common_page_data
+    
     form = FileUploadForm(course=common_page_data['course'])
 
     return render(request, 'files/upload.html',
             {'common_page_data': common_page_data,
              'form': form,
              })
+
+@auth_is_course_admin_view_wrapper
+def edit(request, course_prefix, course_suffix, file_id):
+
+    common_page_data=request.common_page_data
+
+    try:
+        file=File.objects.get(id=file_id, course=common_page_data['draft_course'])
+    except File.DoesNotExist:
+        raise Http404
+                         
+    form = FileEditForm(instance=file, course=common_page_data['course'])
+    return render(request, 'files/upload.html',
+                      {'common_page_data': common_page_data,
+                      'form': form,
+                  })
