09b34ac52b1f16c203fa1620f3722edf1d670db1
Sef Kloninger <sef@kloninger.com>
2013-01-09 16:39:40 -0800

    interactive autograder tests (fix #1726)

--- main/courses/exams/tests.py
+++ main/courses/exams/tests.py
@@ -5,10 +5,13 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 import random
+import re
+import urllib,urllib2
 from django.test import TestCase
+from django.conf import settings
 from courses.exams.autograder import *
 from sets import Set
-
+from StringIO import StringIO
 
 class SimpleTest(TestCase):
     def test_multiple_choice_factory_normal(self):
@@ -332,4 +335,121 @@ class SimpleTest(TestCase):
         self.assertEqual(agf.grade('q4d',"3.5"), {'correct':False,'score':-23})
         self.assertFalse(agf.grade('q4d',"3.0")['correct'])
         self.assertFalse(agf.grade('randomDNE',"33")['correct']) #This is the actual test
+
+
+    def test_interactive_grader_required_elements(self):
+        """Interactive grader requires some XML elements"""
+
+        valid_xml="""
+<exam_metadata>
+    <question_metadata id="q1" data-report="q1">
+        <solution></solution>
+        <response name="q1b" answertype="dbinteractiveresponse">
+            <grader_name>SQL_Grader_schroot</grader_name>
+            <database-file>sql-social-query2.db</database-file>
+            <answer-file>sql-social-query-ans2.txt</answer-file>
+            <select_dict></select_dict>
+            <parameters>
+                <qnum>1</qnum>
+                <answer-text>Enter your SQL query here</answer-text>
+            </parameters>
+        </response>
+    </question_metadata>
+</exam_metadata>"""
+
+        elements = ["database-file", "parameters", "select_dict", "answer-file"]
+        for elem in elements:
+            open_tag = "<" + elem + ">"
+            close_tag = "</" + elem + ">"
+            # re.S flag so the regexp can match newlines between elements
+            partial_xml = re.sub("%s.*%s" % (open_tag, close_tag), "", valid_xml, flags=re.S)
+            with self.assertRaisesRegexp(AutoGraderMetadataException,
+                    ".*A <%s> element is required" % elem):
+                AutoGrader(partial_xml)
+
+
+    def test_interactive_grader_basic(self):
+        """
+        Create and use basic interactive autograder
+
+        Uses some XML from a db class interactive exercise, but the actual values
+        aren't used since we just fake out the endpoint.
+
+        Method cribbed from http://stackoverflow.com/questions/2276689/how-do-i-unit-test-a-module-that-relies-on-urllib2
+        """
+        def fake_remote_grader(answer):
+            """Helper function for the interactive grader, override the remote
+            grader to return the answer string provided."""
+
+            def fake_remote_grader(req):
+                grader_endpoint = getattr(settings, 'GRADER_ENDPOINT', 'localhost')
+                if req.get_full_url() == grader_endpoint:
+                    resp = urllib2.addinfourl(StringIO(answer), "", req.get_full_url())
+                    resp.code = 200
+                    resp.msg = ""
+                    return resp
+
+            class FakeGraderHTTPHandler(urllib2.HTTPHandler):
+                def http_open(self, req):
+                    return fake_remote_grader(req)
+
+            my_opener = urllib2.build_opener(FakeGraderHTTPHandler)
+            urllib2.install_opener(my_opener)
+
+        interactive_xml = """
+<exam_metadata>
+
+    <question_metadata id="q1" data-report="q1">
+        <solution></solution>
+        <response name="q1b" answertype="dbinteractiveresponse">
+            <grader_name>SQL_Grader_schroot</grader_name>
+            <database-file>sql-social-query2.db</database-file>
+            <answer-file>sql-social-query-ans2.txt</answer-file>
+            <select_dict></select_dict>
+            <parameters>
+                <qnum>1</qnum>
+                <answer-text>Enter your SQL query here</answer-text>
+            </parameters>
+        </response>
+    </question_metadata>
+
+    <question_metadata id="q2" data-report="q2">
+        <solution></solution>
+        <response name="q2b" answertype="dbinteractiveresponse">
+            <grader_name>SQL_Grader_schroot</grader_name>
+            <database-file>sql-social-query2.db</database-file>
+            <answer-file>sql-social-query-ans2.txt</answer-file>
+            <select_dict></select_dict>
+            <parameters>
+                <qnum>2</qnum>
+                <answer-text>Enter your SQL query here</answer-text>
+            </parameters>
+        </response>
+    </question_metadata>
+
+</exam_metadata>"""
+
+        ag = AutoGrader(interactive_xml)
+
+        fake_remote_grader("""{"score":0, "maximum-score":1, "feedback":[ {"user_answer":"ignored", "score":0, "explanation":"bummer, try again" }]}""")
+        self.assertTrue(ag.grade("q1b", "should_fail"),
+                {'correct': False, 'score': 0, 'feedback': "bummer, try again"})
+
+        fake_remote_grader("""{"score":1, "maximum-score":1, "feedback":[ {"user_answer":"ignored", "score":0, "explanation":"score 1 of 1" }]}""")
+        self.assertTrue(ag.grade("q1b", "should_succeed"),
+                {'correct': True, 'score': 1, 'feedback': "score 1 of 1"})
+
+        fake_remote_grader("""{"score":10, "maximum-score":10, "feedback":[ {"user_answer":"ignored", "score":0, "explanation":"score 10 of 10" }]}""")
+        self.assertTrue(ag.grade("q1b", "should_succeed"),
+                {'correct': True, 'score': 1, 'feedback': "score 10 of 10"})
+        self.assertTrue(ag.grade("q2b", "should_succeed"),
+                {'correct': True, 'score': 1, 'feedback': "score 10 of 10"})
+
+        fake_remote_grader("""{"score":1, "feedback":[ {"explanation":"score 1, max inferred" }]}""")
+        self.assertTrue(ag.grade("q1b", "should_succeed"),
+                {'correct': True, 'score': 1, 'feedback': "score 1, max inferred"})
+
+        fake_remote_grader("""{"score":0, "feedback":[ {"explanation":"score 0, max inferred" }]}""")
+        self.assertTrue(ag.grade("q1b", "should_fail"),
+                {'correct': False, 'score': 0, 'feedback': "score 0, max inferred"})
 
