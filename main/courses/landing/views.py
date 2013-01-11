5ef4f53df5992c5882d86c3d962432f4e3c7e7eb
Sef Kloninger <sef@kloninger.com>
2013-01-11 12:44:23 -0800

    Add "we're hiring!" page and ribbon (fix #1809)

--- main/courses/landing/views.py
+++ main/courses/landing/views.py
@@ -4,12 +4,16 @@ from django.conf import settings
 
 def landing(request):
     """For normal servers, return our project landing page.  For maint servers,
-    show our maintenance page."""
-
+    show our maintenance page.  Hiring shows a little banner stripe."""
+    hiring=True
     context = RequestContext(request)
     maint_override = getattr(settings, 'MAINTENANCE_LANDING_PAGE', False)
     if maint_override:
-        r = render_to_response('maint.html', context_instance=context)
+        r = render_to_response('maint.html', {'hiring': hiring}, context_instance=context)
     else:
-        r = render_to_response('landing/landing.html', context_instance=context)
+        r = render_to_response('landing/landing.html', {'hiring': hiring}, context_instance=context)
     return r
+
+def hiring(request):
+    context = RequestContext(request)
+    return render_to_response('landing/hiring.html', context_instance=context)
