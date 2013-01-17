2f12d73e321c9b4f2b6a3a59e301bef99349fd15
Sef Kloninger <sef@kloninger.com>
2013-01-17 14:35:46 -0800

    upgrade to storages=1.1.6, boto 2.7.0 (#1923)

--- chef/cookbooks/class2go-python/recipes/default.rb
+++ chef/cookbooks/class2go-python/recipes/default.rb
@@ -1,14 +1,18 @@
 # Sef 7-Aug-2012:
-# Initial attempt to install with easy_install caused cryptic failures
+# Initial attempt to install with pip caused cryptic failures
 # libreadline.so.5 not found...  
 # possibly some interop problem with the quirky bitnami python setup?
 # don't know but easy_install works, so let's go with that for now.
 
 easy_install_package "django-storages" do
+    options "-U"
+    version "1.1.6"
     action :install
 end
 
 easy_install_package "boto" do
+    options "-U"
+    version "2.7.0"
     action :install
 end
 
