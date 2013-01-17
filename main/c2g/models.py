cf0d014ae5aa11d0f8dc06c4ed82bcf8379faf24
Joe Blaylock <jrbl@jrbl.org>
2013-01-17 11:32:48 -0800

    Make ContentGroup.groupinfo_by_id more defensive

--- main/c2g/models.py
+++ main/c2g/models.py
@@ -2378,7 +2378,7 @@ class ContentGroup(models.Model):
         OTOH, if ContentGroup.get_content_type becomes constant-time, this
         becomes linear, and then we win.
         """
-        info = {}
+        info = {'__parent': None, '__parent_tag': '', '__children': [], '__group_id': None, }
         cls = thisclass.groupable_types.get(tag, False)
         if not cls:
             return info
@@ -2387,7 +2387,8 @@ class ContentGroup(models.Model):
         try:
             cgobjs = ContentGroup.objects.filter(group_id=obj.contentgroup_set.get().group_id)
         except ContentGroup.DoesNotExist:
-            return info
+            return {}
+        info['__group_id'] = cgobjs[0].group_id
         for cgo in cgobjs:
             cttag = cgo.get_content_type()
             cgref = getattr(cgo, cttag)
@@ -2397,10 +2398,8 @@ class ContentGroup(models.Model):
                 info['__parent'] = cgref
                 info['__parent_tag'] = cttag
             else:
-                info.setdefault('__children', []).append(cgref)
-            info.setdefault(cttag, []).append(cgref)
-        if info:
-            info['__group_id'] = cgobjs[0].group_id
+                info['__children'].append(cgref)
+            info[cttag] = info.get(cttag, []).append(cgref)
         return info
 
     @classmethod
@@ -2408,15 +2407,10 @@ class ContentGroup(models.Model):
         """When updating parent's content section, make children follow it."""
         def do_content_section_update(obj, new_section_ref):
             # NB: ContentGroup objects are all ready-mode instances of a thing.
-            # TODO: Of course the nicer thing here would be to iterate over
-            # the children in the view, calling 
-            # "child.commit(('section', new_section))" and have the commit
-            # function just know how to iterate a set of tuples and DTRT with
-            # individual updates. But until we have that, we're doing this.
             obj.section = new_section_ref
             obj.save()
             if hasattr(obj, 'image'):
-                if new_section_ref:
+                if new_section_ref != None:      # can't take .image() of None
                     obj.image.section = new_section_ref.image
                 else:
                     obj.image.section = new_section_ref
@@ -2430,15 +2424,13 @@ class ContentGroup(models.Model):
         if new_section_ref and new_section_ref.mode != "ready":
             new_section_ref = new_section_ref.image
         # * Get the parent information if it's available...
-        if cginfo.has_key('__parent'):
-            group_parent = cginfo['__parent']
+        group_parent = cginfo['__parent']
         # * ...but if it's not, this call should no-op
-        else:
+        if not group_parent:
             return False
         # * If this CG is already in new_section, this call should no-op
         if group_parent.section and new_section_ref and new_section_ref.id == group_parent.section.id:
             return False
-        # * Don't care about moving children to diff section, child CG update will take care of that
         # * Otherwise "normal" case of group w/ parents and children getting moved
         do_content_section_update(group_parent, new_section_ref)
         for child in cginfo['__children']:
