f77b5ed8782e2e1d0bbec88f484de954549caeab
Joe Blaylock <jrbl@jrbl.org>
2013-01-29 18:45:20 -0800

    Bugfix to certificate asset downloader
    * Target directories weren't being created, so if they didn't already
      exist, the download would fail with an exception. Now we recursively
      create any missing directories in the target hierarchy.

--- main/certificates/management/commands/get_cert_assets.py
+++ main/certificates/management/commands/get_cert_assets.py
@@ -2,11 +2,24 @@ import os
 from storages.backends.s3boto import S3BotoStorage
 
 from django.conf import settings
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 
 
 release_storage = S3BotoStorage(bucket=settings.AWS_RELEASE_BUCKET_NAME, access_key=settings.AWS_ACCESS_KEY_ID, secret_key=settings.AWS_SECRET_ACCESS_KEY)
 
+
+def _mkdir(newdir):
+    if os.path.isdir(newdir):
+        return
+    elif os.path.isfile(newdir):
+        raise CommandError("Couldn't make directory %s; a file with the same name as '%s' already exists." % (newdir, newdir))
+    else:
+        head, tail = os.path.split(newdir)
+        if head and not os.path.isdir(head):
+            _mkdir(head)
+        if tail:
+            os.mkdir(newdir)
+    return
 
 def get_files_to_copy():
     to_copy = []
@@ -27,6 +40,8 @@ def copy_file(bucket_path):
     local_dir = settings.MEDIA_ROOT
     tmp_file = release_storage.open(bucket_path, 'rb')
     local_filepath = local_dir + bucket_path[13:]  # trim off leading /certificates
+    making_target, filename = os.path.split(local_filepath)
+    _mkdir(making_target)
     local_file = open(local_filepath, 'wb') 
     local_file.write(tmp_file.read())
     tmp_file.close()
