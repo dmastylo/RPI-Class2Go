7d08866187bc06a6606fa5a18a9daf69c422529d
Joe Blaylock <jrbl@jrbl.org>
2013-03-05 10:10:20 -0800

    Documentation fixes on s3boto monkeypatch

--- main/monkeypatch.py
+++ main/monkeypatch.py
@@ -5,16 +5,23 @@ But then submit your code upstream. While you're waiting for review, add a
 monkeypatch here.
 """
 
+
 def s3boto_dlurl(self, name, response_headers=None, querystring_auth=True):
-    """Just like s3boto.url(), but it supports response_headers setting.
+    """Copy s3boto.S3BotoStorage.url() implementation with flexibility.
     
-    This method is monkeypatched onto the class class2go/main/monkeypatch.py
+    response_headers is normally unsupported, but useful for creating URLs
+    that point to resources with peculiar configurations, i.e., 
+    'content-disposition': 'attachment'.
+
+    querystring_auth is normally configured on S3BotoStorage via the
+    environment, but defaults to True and it's handy to be able to override on
+    a per-model basis.
     """
     name = self._normalize_name(self._clean_name(name))
     if self.custom_domain:
         return "%s://%s/%s" % ('https' if self.secure_urls else 'http',
                                self.custom_domain, name)
-    # only set querystring_auth if response_headers=None
+    # If response_headers are set, use class-default querysting_auth behavior
     if response_headers is not None:
         querystring_auth = self.querystring_auth
 
@@ -24,4 +31,5 @@ def s3boto_dlurl(self, name, response_headers=None, querystring_auth=True):
             query_auth=querystring_auth, force_http=not self.secure_urls)
 import storages.backends.s3boto
 storages.backends.s3boto.S3BotoStorage.url_monkeypatched = s3boto_dlurl
+
 
