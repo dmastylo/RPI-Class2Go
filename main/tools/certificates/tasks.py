90df65ed76d4ae1c1a0825e8cb6ba80b7c92d92a
Joe Blaylock <jrbl@jrbl.org>
2013-03-25 16:54:28 -0700

    Multi-certification initial version
    * Enables a new manage.py target, issue_certificate_multi, which allows
      an entire class to be certified at one go.
    * Introduces two new certification-process configuration file formats,
      which allow (after initial testing), fire-and-forget running of
      certifications.
    * Improved docstrings and other built-in help.
    * Multi-certification runs with the wkhtml-based PDFKit only; xhtml2pdf
      is deprecated.
    * CourseCertificate entries in the database are created on-the-fly from
      the class certificate conditions description file.
    * Students are assigned to various certification conditions based on the
      contents of the CourseStudentScore table and the description in the
      class certificate conditions description file.

--- main/tools/certificates/tasks.py
+++ main/tools/certificates/tasks.py
@@ -26,22 +26,6 @@ def working_file():
     base = getattr(settings, 'FILE_UPLOAD_TEMP_DIR', '/tmp')
     return tempfile.mkstemp(prefix='tmpcert-', suffix='.pdf', dir=base)
 
-def render_template(firstname, lastname, asset_prefix, asset_path, cert_type):
-    infile_name = 'certificate-' + cert_type + '.html'
-    if not asset_prefix: asset_prefix = '/'      # NB: force local asset delivery
-    render_context = Context({'student_name': firstname + ' ' + lastname,
-                              'basepath':     asset_prefix + '/' + asset_path + '/',
-                             })
-    if asset_prefix:              # Local storage
-        infile_path = os.path.join(asset_prefix, asset_path, infile_name)
-        asset_handle = open(infile_path, 'rb')
-    else:                         # S3 storage
-        infile_path = '/' + asset_path + '/' + infile_name
-        asset_handle = release_file_storage.open(infile_path, 'rb')
-    template = Template(asset_handle.read()).render(render_context)
-    asset_handle.close()
-    return template
-
 def upload_certificate(tmp_path, outpath_prefix, path, outfile_name):
     if outpath_prefix:            # Local storage
         path = os.path.join(outpath_prefix, path)
@@ -60,7 +44,7 @@ def upload_certificate(tmp_path, outpath_prefix, path, outfile_name):
     return storage_name
 
 @task
-def certify(path_prefix, course, certificate, student):
+def certify(path_prefix, course, certificate, student, force_pdfkit=False):
     if not certificate.assets or not certificate.storage:
         raise ValueError("Certificate %s incorrectly specified; bad file paths" % str(certificate))
     # It is an error to call this method without an html to pdf renderer installed
@@ -70,13 +54,22 @@ def certify(path_prefix, course, certificate, student):
     # Asset prefix forced to local b/c xhtml2pdf can't read out of s3
     asset_prefix = getattr(settings, 'MEDIA_ROOT', '/')
 
+    # Read in PDF template and set up render context
+    infile_name = 'certificate-' + certificate.type + '.html'
+    if not asset_prefix: asset_prefix = '/'      # NB: force local asset delivery
+    infile_path = os.path.join(asset_prefix, certificate.assets, infile_name)
+    unrendered_template = open(infile_path, 'rb').read()
+
     # Read in and render the template for this student name
-    pdf_src = render_template(student.first_name, student.last_name, asset_prefix, certificate.assets, certificate.type)
+    render_context = Context({'student_name': student.first_name + ' ' + student.last_name,
+                              'basepath':     asset_prefix + '/' + certificate.assets + '/',
+                             })
+    pdf_src = Template(unrendered_template).render(render_context)
 
     # create the PDF in a safe temporary location
     tmp_handle, tmp_path = working_file()
-    if pdfkit:
-        pdf_gen_options = {'page-size': 'a4', 'encoding': 'UTF-8', 'orientation': 'landscape', 'margin-left': '0', 'margin-right': '0', 'margin-top': '0', 'margin-bottom': '0', 'quiet': ''}
+    if pdfkit or force_pdfkit:
+        pdf_gen_options = {'page-size': 'letter', 'encoding': 'UTF-8', 'orientation': 'landscape', 'margin-left': '0', 'margin-right': '0', 'margin-top': '0', 'margin-bottom': '0', 'quiet': ''}
         it_worked = pdfkit.from_string(input=pdf_src, output_path=tmp_path, options=pdf_gen_options)
         if not it_worked:
             # Except as far as I can tell, usually pdfkit will throw an exception and we'll die before we get here
@@ -88,6 +81,30 @@ def certify(path_prefix, course, certificate, student):
 
     # After file creation, move to s3 (or local)
     outfile_name = certificate.get_filename_by_user(student)
-    uploaded_to = upload_certificate(tmp_path, path_prefix, certificate.storage, outfile_name)
+    return upload_certificate(tmp_path, path_prefix, certificate.storage, outfile_name)
 
-    return uploaded_to
+@task
+def makePDF(templatestr, path_prefix, course, certificate, student, context_in={}):
+    # It is an error to call this method without an html to pdf renderer installed
+    if not pisa and not pdfkit:
+        raise ValueError("Certification cannot proceed without pdfkit installed.")
+
+    # Render the template for this student name
+    asset_prefix = getattr(settings, 'MEDIA_ROOT', '/')
+    context_in.update({'student_name': student.first_name + ' ' + student.last_name,
+                    'basepath':     os.path.join(asset_prefix, certificate.assets, ''), })
+    render_context = Context(context_in)
+    pdf_src = Template(templatestr).render(render_context)
+
+    # create the PDF in a safe temporary location
+    tmp_handle, tmp_path = working_file()
+    pdf_gen_options = {'page-size': 'letter', 'encoding': 'UTF-8', 'orientation': 'landscape', 'margin-left': '0', 'margin-right': '0', 'margin-top': '0', 'margin-bottom': '0', 'quiet': ''}
+    it_worked = pdfkit.from_string(input=pdf_src, output_path=tmp_path, options=pdf_gen_options)
+    if not it_worked:
+        # Except as far as I can tell, usually pdfkit will throw an exception and we'll die before we get here
+        raise ValueError("PDFKit yielded error for user certification %s, %s" % (student.username, certificate.type))
+
+    # After file creation, move to s3 (or local)
+    outfile_name = certificate.get_filename_by_user(student)
+    return upload_certificate(tmp_path, path_prefix, certificate.storage, outfile_name)
+
