087b077f152962a76c13956dfd293292c84103fd
Sef Kloninger <sef@kloninger.com>
2013-03-28 23:26:15 -0700

    exam_edit: option parsing and validation (#2459)

--- main/courses/management/commands/exam_edit.py
+++ main/courses/management/commands/exam_edit.py
@@ -5,6 +5,7 @@ except ImportError, msg:
 from optparse import make_option
 import inspect
 from textwrap import wrap
+from collections import namedtuple
 
 from django.core.management.base import BaseCommand, CommandError
 from c2g.models import Exam
@@ -16,18 +17,36 @@ class Command(BaseCommand):
     exam_attrs = [a for a in vars(testexam) \
                     if not callable(getattr(testexam,a)) \
                     and not a.startswith('_')]
+    exam_types = [t[0] for t in Exam.EXAM_TYPE_CHOICES]
 
-    help = "Make bulk exam changes. With the -u option update the database. PLEASE BE CAREFUL." \
-        "\n\nThe list of Exam columns are:\n%s" % "\n".join(wrap(", ".join(sorted(exam_attrs))))
+    help = "Make bulk exam changes. With the -u option update the database. " \
+            " PLEASE BE CAREFUL." \
+            "\n\nSelect which exams to change with one or more of " \
+            "-e, -c, and -t. At least one of -e or -c must be used." \
+            "\n\nThe list of Exam columns are:\n%s" % "\n".join(wrap(", ".join(sorted(exam_attrs))))
 
     option_list = (
-        make_option("-u", "--update", action="store_false", dest="dryrun", default=True,
-            help="actually update database (default is dry run)."),
+        # Select
+        make_option("-e", "--examids", dest="exam_ids", type="string",
+            help="Select by comma-separated list of exam ID's"),
+        make_option("-c", "--courseid", dest="course_id", type="int", 
+            help="Select by course.  If only this option is chosen, all exams " \
+                    "for that course will be selected."),
+        make_option("-t", "--type", dest="type", type="string",
+            help="Select by type, valid values are: %s" \
+                    % ", ".join(sorted(exam_types))),
+
+        # Change
         make_option("-s", "--set", action="append", dest="setlist", 
             default=[], metavar="COL=\"VAL\"",
             help="Set this to that for every exam that matches your search. "  \
                  "Specify this multiple times to update multiple columns. " \
                  "The quotes around the value are optional."),
+
+        # Do It!
+        make_option("-u", "--update", action="store_false", dest="dryrun", default=True,
+            help="actually update database (default is dry run)."),
+
     ) + BaseCommand.option_list
 
 #        make_option("--start", dest="start_time",
@@ -35,6 +54,44 @@ class Command(BaseCommand):
 #        make_option("--end", dest="end_time",
 #            help="consider entries no later than X"),
 
-    def handle(self, *args, **options):
-        pass
+    def validate_options(self, options):
+        """
+        Validate options and parse into useful data types. Returns a namedtuple.
+        """
+        if not (options['exam_ids'] or options['course_id']):
+            raise CommandError("At least one of exam_ids (-e) or course_id (-c) is required.")
 
+        Selector = namedtuple('Selector', 'exam_ids, course_id, type')
+
+        result_exam_id_list = []
+        if options['exam_ids']:
+            exid_strings = options['exam_ids'].split(',')
+            for exid_str in exid_strings:
+                errstr = None
+                try:
+                    exid = int(exid_str)
+                    if exid == 0:
+                        errstr = "exam id \"%s\" is invalid"
+                except ValueError as e:
+                    errstr = e
+                if errstr:
+                    raiseCommandError("Exam ID parsing error, %s" % errstr)
+                result_exam_id_list.append(exid)
+
+        result_type = None
+        if options['type']:
+            if options['type'] not in self.exam_types:
+                raise CommandError("Invalid exam type \"%s\" given, allowed types are: %s"
+                    % (options['type'], ", ".join(sorted(self.exam_types))))
+            result_type = options['type']
+
+        return Selector(exam_ids=result_exam_id_list, 
+                course_id=options['course_id'], 
+                type=result_type)
+
+
+    def handle(self, *args, **options):
+        sel = self.validate_options(options)
+
+
+        
