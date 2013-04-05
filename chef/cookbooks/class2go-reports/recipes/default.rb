e6787af7153df17bc9cd36d3658668822af29c9c
Sef Kloninger <sef@kloninger.com>
2013-04-05 17:42:06 +0000

    stop generat+send daily gnuplot graphs (fix #2510)

--- chef/cookbooks/class2go-reports/recipes/default.rb
+++ chef/cookbooks/class2go-reports/recipes/default.rb
@@ -6,9 +6,9 @@ package "mpack" do
     action :install
 end
 
-package "gnuplot" do
-    action :install
-end
+# package "gnuplot" do
+#     action :install
+# end
 
 template "mysql-client-config" do
     path node['system']['admin_home'] + "/.my.cnf"
@@ -18,19 +18,19 @@ template "mysql-client-config" do
     mode 00644
 end
 
-cron "users_by_class" do
-    hour "10"   # GMT
-    minute "0"
-    user node['system']['admin_user']
-    command "(cd #{node['system']['admin_home']}/class2go/reports; ./users_by_class.sh class2go-nightly-reports@cs.stanford.edu)"
-end
-
-cron "users_by_day" do
-    hour "10"   # GMT
-    minute "10"
-    user node['system']['admin_user']
-    command "(cd #{node['system']['admin_home']}/class2go/reports; ./users_by_day.sh class2go-nightly-reports@cs.stanford.edu)"
-end
+# cron "users_by_class" do
+#     hour "10"   # GMT
+#     minute "0"
+#     user node['system']['admin_user']
+#     command "(cd #{node['system']['admin_home']}/class2go/reports; ./users_by_class.# sh class2go-nightly-reports@cs.stanford.edu)"
+# end
+# 
+# cron "users_by_day" do
+#     hour "10"   # GMT
+#     minute "10"
+#     user node['system']['admin_user']
+#     command "(cd #{node['system']['admin_home']}/class2go/reports; ./users_by_day.# sh class2go-nightly-reports@cs.stanford.edu)"
+# end
 
 cron "generate main reports" do
     hour "10"  # GMT
