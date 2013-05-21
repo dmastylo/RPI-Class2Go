94771a10852843f28665897471b7fcfef7047da7
Sef Kloninger <sef@kloninger.com>
2013-05-21 01:07:29 -0700

    check mails, ensure lower case
    Small tweaks made for the May 2013 mail blast.

--- tools/mail-blast/merge.py
+++ tools/mail-blast/merge.py
@@ -3,7 +3,9 @@
 import csv
 import random
 
-OUTPUT="dec12_list.csv"
+OUTPUT="merged-may13.csv"
+CHECKLINE=",sefklon+%03d@gmail.com\n"
+CHECKS=10000
 
 def dict_from_file(fname):
     result = dict()
@@ -13,16 +15,18 @@ def dict_from_file(fname):
     for row in reader:
         if len(row) == 2: 
             (name,email) = row
-            result[email] = name
+            result[email.lower()] = name
+        elif len(row) == 1: 
+            result[row[0].lower()] = ''
         else:
             skips += 1
     print "%s:\t read %d\tskipped %d" % (fname, len(result), skips)
     return result 
 
 def main():
-    mainlist = dict_from_file("sep12.csv")
-    lastquarter = dict_from_file("authuser-dec12.csv")
-    excludes = dict_from_file("excludes-dec12.csv")
+    mainlist = dict_from_file("starting-may13.csv")
+    lastquarter = dict_from_file("authuser-may13.csv")
+    excludes = dict_from_file("excludes-may13.csv")
 
     newlist = dict()
     for k,v in mainlist.iteritems():
@@ -36,8 +40,21 @@ def main():
 
     f = open(OUTPUT, 'w')
     line = 0
+    written = 0
+    checklines = 0
+
+    f.write(CHECKLINE % checklines)
+    checklines += 1
     for k in newlist_k:
         f.write("%s,%s\n" % (newlist[k],k))
+        written += 1
+        if written % CHECKS == 0:
+            f.write(CHECKLINE % checklines)
+            checklines += 1
+    f.write(CHECKLINE % checklines)
+    checklines += 1
+
+    print "%s:\t wrote %d\tchecklines %d" % (OUTPUT, written, checklines)
     
 if __name__ == '__main__':
     main()
